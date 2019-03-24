84261f85bde2fd815fb1f20ed0a8f890a61f90e7
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2019-03-24 11:52:25 +0100

    Add D+ in Ds+ channel

--- ProjectDplusDsSparse.py
+++ ProjectDplusDsSparse.py
@@ -21,11 +21,17 @@ infileData = TFile(inputCfg['filename'])
 indirData = infileData.Get(inputCfg['dirname'])
 inlistData = indirData.Get(inputCfg['listname'])
 sMassPtCutVars = inlistData.FindObject(inputCfg['sparsenameAll'])
+enableSecPeak = inputCfg['enableSecPeak']
 if isMC:
     sMassPtCutVarsPrompt = inlistData.FindObject(inputCfg['sparsenamePrompt'])
     sMassPtCutVarsFD = inlistData.FindObject(inputCfg['sparsenameFD'])
     sGenPrompt = inlistData.FindObject(inputCfg['sparsenameGenPrompt'])
     sGenFD = inlistData.FindObject(inputCfg['sparsenameGenFD'])
+    if enableSecPeak:
+        sMassPtCutVarsPromptSecPeak = inlistData.FindObject(inputCfg['sparsenamePromptSecPeak'])
+        sMassPtCutVarsFDSecPeak = inlistData.FindObject(inputCfg['sparsenameFDSecPeak'])
+        sGenPromptSecPeak = inlistData.FindObject(inputCfg['sparsenameGenPromptSecPeak'])
+        sGenFDSecPeak = inlistData.FindObject(inputCfg['sparsenameGenFDSecPeak'])
 normCounter = indirData.Get(inputCfg['normname'])
 hEv = inlistData.FindObject(inputCfg['histoevname'])
 
@@ -46,6 +52,10 @@ for iPt in range(0,len(cutVars['Pt']['min'])):
         if isMC:
             sMassPtCutVarsPrompt.GetAxis(cutVars[iVar]['axisnum']).SetRange(binMin,binMax)
             sMassPtCutVarsFD.GetAxis(cutVars[iVar]['axisnum']).SetRange(binMin,binMax)
+            if enableSecPeak:
+                sMassPtCutVarsPromptSecPeak.GetAxis(cutVars[iVar]['axisnum']).SetRange(binMin,binMax)
+                sMassPtCutVarsFDSecPeak.GetAxis(cutVars[iVar]['axisnum']).SetRange(binMin,binMax)
+
     for iVar in cutVars:
         hVar = sMassPtCutVars.Projection(cutVars[iVar]['axisnum'])
         hVar.SetName('h%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
@@ -58,6 +68,13 @@ for iPt in range(0,len(cutVars['Pt']['min'])):
             hVarFD = sMassPtCutVarsFD.Projection(cutVars[iVar]['axisnum'])
             hVarFD.SetName('hFD%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
             hVarFD.Write()
+            if enableSecPeak:
+                hVarPromptSecPeak = sMassPtCutVarsPromptSecPeak.Projection(cutVars[iVar]['axisnum'])
+                hVarPromptSecPeak.SetName('hPromptSecPeak%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
+                hVarPromptSecPeak.Write()
+                hVarFDSecPeak = sMassPtCutVarsFDSecPeak.Projection(cutVars[iVar]['axisnum'])
+                hVarFDSecPeak.SetName('hFDSecPeak%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
+                hVarFDSecPeak.Write()
     if isMC:
         binGenMin = sGenPrompt.GetAxis(0).FindBin(cutVars['Pt']['min'][iPt]*1.0001)
         binGenMax = sGenPrompt.GetAxis(0).FindBin(cutVars['Pt']['max'][iPt]*0.9999)
@@ -69,6 +86,15 @@ for iPt in range(0,len(cutVars['Pt']['min'])):
         hGenPtFD = sGenFD.Projection(0)
         hGenPtFD.SetName('hFDGenPt_%0.f_%0.f' % (cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
         hGenPtFD.Write()
+        if enableSecPeak:
+            sGenPromptSecPeak.GetAxis(0).SetRange(binGenMin,binGenMax)
+            sGenFDSecPeak.GetAxis(0).SetRange(binGenMin,binGenMax)
+            hGenPtPromptSecPeak = sGenPromptSecPeak.Projection(0)
+            hGenPtPromptSecPeak.SetName('hPromptSecPeakGenPt_%0.f_%0.f' % (cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
+            hGenPtPromptSecPeak.Write()
+            hGenPtFDSecPeak = sGenFDSecPeak.Projection(0)
+            hGenPtFDSecPeak.SetName('hFDSecPeakGenPt_%0.f_%0.f' % (cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
+            hGenPtFDSecPeak.Write()
 
 hEvForNorm = TH1F("hEvForNorm",";;Number of events",2,0.,2.)
 hEvForNorm.GetXaxis().SetBinLabel(1,"norm counter")
