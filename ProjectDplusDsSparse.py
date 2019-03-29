44d4af3f97a72d46381df92335a08ae26b7fe3d4
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2019-03-29 09:32:26 +0100

    Project only mass and pt histograms to save time

--- ProjectDplusDsSparse.py
+++ ProjectDplusDsSparse.py
@@ -43,6 +43,7 @@ cutVars = cutSetCfg['cutvars']
 outfile = TFile(outFileName,'RECREATE')
 
 for iPt in range(0,len(cutVars['Pt']['min'])):
+    print("Projecting distributions for %0.f < pT < %0.f GeV/c" % (cutVars['Pt']['min'][iPt],cutVars['Pt']['max'][iPt]))
     for iVar in cutVars:
         if iVar == 'InvMass':
             continue
@@ -57,24 +58,25 @@ for iPt in range(0,len(cutVars['Pt']['min'])):
                 sMassPtCutVarsFDSecPeak.GetAxis(cutVars[iVar]['axisnum']).SetRange(binMin,binMax)
 
     for iVar in cutVars:
-        hVar = sMassPtCutVars.Projection(cutVars[iVar]['axisnum'])
-        hVar.SetName('h%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
-        outfile.cd()
-        hVar.Write()
-        if isMC:
-            hVarPrompt = sMassPtCutVarsPrompt.Projection(cutVars[iVar]['axisnum'])
-            hVarPrompt.SetName('hPrompt%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
-            hVarPrompt.Write()
-            hVarFD = sMassPtCutVarsFD.Projection(cutVars[iVar]['axisnum'])
-            hVarFD.SetName('hFD%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
-            hVarFD.Write()
-            if enableSecPeak:
-                hVarPromptSecPeak = sMassPtCutVarsPromptSecPeak.Projection(cutVars[iVar]['axisnum'])
-                hVarPromptSecPeak.SetName('hPromptSecPeak%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
-                hVarPromptSecPeak.Write()
-                hVarFDSecPeak = sMassPtCutVarsFDSecPeak.Projection(cutVars[iVar]['axisnum'])
-                hVarFDSecPeak.SetName('hFDSecPeak%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
-                hVarFDSecPeak.Write()
+        if iVar == 'InvMass' or iVar == 'Pt':
+            hVar = sMassPtCutVars.Projection(cutVars[iVar]['axisnum'])
+            hVar.SetName('h%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
+            outfile.cd()
+            hVar.Write()
+            if isMC:
+                hVarPrompt = sMassPtCutVarsPrompt.Projection(cutVars[iVar]['axisnum'])
+                hVarPrompt.SetName('hPrompt%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
+                hVarPrompt.Write()
+                hVarFD = sMassPtCutVarsFD.Projection(cutVars[iVar]['axisnum'])
+                hVarFD.SetName('hFD%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
+                hVarFD.Write()
+                if enableSecPeak:
+                    hVarPromptSecPeak = sMassPtCutVarsPromptSecPeak.Projection(cutVars[iVar]['axisnum'])
+                    hVarPromptSecPeak.SetName('hPromptSecPeak%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
+                    hVarPromptSecPeak.Write()
+                    hVarFDSecPeak = sMassPtCutVarsFDSecPeak.Projection(cutVars[iVar]['axisnum'])
+                    hVarFDSecPeak.SetName('hFDSecPeak%s_%0.f_%0.f' % ( cutVars[iVar]['name'], cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
+                    hVarFDSecPeak.Write()
     if isMC:
         binGenMin = sGenPrompt.GetAxis(0).FindBin(cutVars['Pt']['min'][iPt]*1.0001)
         binGenMax = sGenPrompt.GetAxis(0).FindBin(cutVars['Pt']['max'][iPt]*0.9999)
@@ -96,11 +98,20 @@ for iPt in range(0,len(cutVars['Pt']['min'])):
             hGenPtFDSecPeak.SetName('hFDSecPeakGenPt_%0.f_%0.f' % (cutVars['Pt']['min'][iPt] , cutVars['Pt']['max'][iPt]))    
             hGenPtFDSecPeak.Write()
 
+    for iVar in cutVars:
+        sMassPtCutVars.GetAxis(cutVars[iVar]['axisnum']).SetRange(-1,-1)
+        if isMC:
+            sMassPtCutVarsPrompt.GetAxis(cutVars[iVar]['axisnum']).SetRange(-1,-1)
+            sMassPtCutVarsFD.GetAxis(cutVars[iVar]['axisnum']).SetRange(-1,-1)
+            if enableSecPeak:
+                sMassPtCutVarsPromptSecPeak.GetAxis(cutVars[iVar]['axisnum']).SetRange(-1,-1)
+                sMassPtCutVarsFDSecPeak.GetAxis(cutVars[iVar]['axisnum']).SetRange(-1,-1)
+
 hEvForNorm = TH1F("hEvForNorm",";;Number of events",2,0.,2.)
 hEvForNorm.GetXaxis().SetBinLabel(1,"norm counter")
 hEvForNorm.GetXaxis().SetBinLabel(2,"accepted events")
 hEvForNorm.SetBinContent(1,normCounter.GetNEventsForNorm())
-hEvForNorm.SetBinContent(2,hEv.GetBinContent(10))
+hEvForNorm.SetBinContent(2,hEv.GetBinContent(5))
 
 hEvForNorm.Write()
 outfile.Close()
