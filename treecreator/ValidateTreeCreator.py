f5298a2d583f662c63aa5d6806af2ad7f2861b4d
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2019-08-21 23:29:57 +0200

    Add code to run and validate tree creator (for Ds)

---
+++ treecreator/ValidateTreeCreator.py
@@ -0,0 +1,115 @@
+import argparse
+import pandas as pd
+import numba
+import six
+import uproot
+from root_numpy import fill_hist # pylint: disable=import-error, no-name-in-module
+from ROOT import TFile, TCanvas, TH1F, TLegend # pylint: disable=import-error, no-name-in-module
+from ROOT import gStyle, kBlack, kRed, kFullCircle, kOpenSquare # pylint: disable=import-error, no-name-in-module
+
+# pylint: disable=invalid-name
+def SetHistoStyle(histo, color, marker, markersize=1, lineewidth=2):
+    histo.SetLineWidth(lineewidth)
+    histo.SetLineColor(color)
+    histo.SetMarkerColor(color)
+    histo.SetMarkerStyle(marker)
+    histo.SetMarkerSize(markersize)
+
+@numba.njit
+def FidAccSel(arrayPt, arrayY):
+    arrayIsSel = []
+    for icand, pt in enumerate(arrayPt):
+        if pt > 5:
+            if abs(arrayY[icand]) < 0.8:
+                arrayIsSel.append(True)
+            else:
+                arrayIsSel.append(False)
+        else:
+            yfid = -0.2/15 * pt**2 + 1.9/15 * pt + 0.5
+            if abs(arrayY[icand]) < yfid:
+                arrayIsSel.append(True)
+            else:
+                arrayIsSel.append(False)
+    return arrayIsSel
+
+# Main function
+gStyle.SetPadRightMargin(0.035)
+gStyle.SetPadLeftMargin(0.15)
+gStyle.SetPadTopMargin(0.05)
+gStyle.SetTitleSize(0.045, 'xy')
+gStyle.SetLabelSize(0.040, 'xy')
+gStyle.SetPadTickX(1)
+gStyle.SetPadTickY(1)
+gStyle.SetLegendBorderSize(0)
+gStyle.SetOptStat(0)
+
+#Variables to be checked
+Vars = {'inv_mass':0}
+
+parser = argparse.ArgumentParser(description='Arguments')
+parser.add_argument('--mc', action='store_true', help='flag for MC outputs')
+parser.add_argument('inputfile', metavar='text', help='input root file name')
+parser.add_argument('inputdir', metavar='text', help='task directory name')
+parser.add_argument('inputlist', metavar='text', help='task list name')
+args = parser.parse_args()
+
+if args.mc:
+    print('MC validation not yet implemented. Exit')
+    exit()
+
+hMassTask, hMassTreeCreator, cComparison = ({} for iDic in range(3))
+
+infile = TFile.Open(args.inputfile)
+listTask = infile.Get('{0}/{1}'.format(args.inputdir, args.inputlist))
+hEv = listTask.FindObject('hNEvents')
+sparse = listTask.FindObject('fnSparse')
+for iVar in Vars:
+    hMassTask[iVar] = sparse.Projection(Vars[iVar])
+    SetHistoStyle(hMassTask[iVar], kBlack, kFullCircle)
+
+treeD = uproot.open(args.inputfile)['PWGHF_TreeCreator/tree_Ds'] #tree_event_char
+dfD = treeD.pandas.df()
+treeEv = uproot.open(args.inputfile)['PWGHF_TreeCreator/tree_event_char']
+dfEv = treeEv.pandas.df()
+dfEvSel = dfEv.query('is_ev_rej==0')
+
+dfMerged = pd.merge(dfD, dfEvSel, on=['run_number', 'ev_id'])
+
+isSel = FidAccSel(dfMerged.pt_cand.values, dfMerged.y_cand.values)
+dfMerged['fidacc'] = isSel
+dfMergedSel = dfMerged.query('fidacc==True')
+
+leg = TLegend(0.4,0.6,0.8,0.8)
+for counter, iVar in enumerate(Vars):
+    nbins = hMassTask[iVar].GetNbinsX()
+    mmin = hMassTask[iVar].GetBinLowEdge(1)
+    mmax = hMassTask[iVar].GetBinLowEdge(nbins)+hMassTask[iVar].GetBinWidth(nbins)
+    hMassTreeCreator[iVar] = TH1F('hMassTreeCreator{0}'.format(iVar), '', nbins, mmin, mmax)
+    fill_hist(hMassTreeCreator[iVar], dfMergedSel[iVar].values)
+    SetHistoStyle(hMassTreeCreator[iVar], kRed, kOpenSquare)
+    cComparison[iVar] = TCanvas('cComparison{0}'.format(iVar), '', 800, 800)
+    ymax = hMassTask[iVar].GetMaximum()*2
+    cComparison[iVar].DrawFrame(mmin, 0.1, mmax, ymax, ';{0}; Counts'.format(iVar))
+    hMassTask[iVar].Draw('Esame')
+    hMassTreeCreator[iVar].Draw('Esame')
+    if counter == 0:
+        leg.AddEntry(hMassTask[iVar], 'task', 'p')
+        leg.AddEntry(hMassTreeCreator[iVar], 'tree creator', 'p')
+    leg.Draw()
+    cComparison[iVar].SaveAs('{0}.pdf'.format(iVar))
+
+hSelEv = TH1F('hEvSel', ';;selected events', 2, 0.5, 2.5)
+hSelEv.GetXaxis().SetBinLabel(1, 'task')
+hSelEv.GetXaxis().SetBinLabel(2, 'tree creator')
+hSelEv.SetBinContent(1, hEv.GetBinContent(5))
+hSelEv.SetBinContent(2, len(dfEvSel))
+SetHistoStyle(hSelEv, kBlack, kFullCircle)
+
+cEvents = TCanvas('cEvents', '', 800, 800)
+hSelEv.Draw('E')
+cEvents.SaveAs('selected_events.pdf')
+
+if six.PY2:
+    raw_input('Press enter to exit')
+elif six.PY3:
+    input('Press enter to exit')
