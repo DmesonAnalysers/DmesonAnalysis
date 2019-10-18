30245bdf436adf4a6b37f939190ff3cc58f766bb
Fabio Catalano <fabio.catalano@cern.ch>
2019-10-18 17:48:28 +0200

    Revamp fit. par. comparison macro

--- CompareFitPars.py
+++ CompareFitPars.py
@@ -1,78 +1,159 @@
+import argparse
 from ROOT import TCanvas, TFile, TLegend, TLine # pylint: disable=import-error,no-name-in-module
-from ROOT import gStyle, kRed, kBlack, kBlue, kOrange, kGreen, kFullCircle, kOpenCircle, kFullSquare, kFullDiamond, kFullCross, kOpenCross # pylint: disable=import-error,no-name-in-module
-import math
-import six
+from ROOT import gStyle, kRed, kBlack, kBlue, kOrange, kGreen # pylint: disable=import-error,no-name-in-module
+from ROOT import kFullCircle, kOpenCircle, kFullSquare, kFullDiamond, kFullCross, kOpenCross # pylint: disable=import-error,no-name-in-module
 
-inputdir = 'outputs/improver'
-inputfilenames = [ 'RawYieldsDs_MC_010_improver.root', 'RawYieldsDs_MC_010_noimprover.root' ]
-colors = [ kRed, kBlue, kBlack ]
-markers = [ kFullCircle, kFullSquare, kFullDiamond ]
-legendnames = [ 'w/ improver', 'w/o improver' ]
-outputsuffix = 'Improver'
+def set_style():
+    gStyle.SetPadRightMargin(0.035)
+    gStyle.SetPadLeftMargin(0.18)
+    gStyle.SetPadTopMargin(0.05)
+    gStyle.SetTitleSize(0.045, 'xy')
+    gStyle.SetLabelSize(0.040, 'xy')
+    gStyle.SetPadTickX(1)
+    gStyle.SetPadTickY(1)
+    gStyle.SetLegendBorderSize(0)
+    gStyle.SetOptStat(0)
 
-hMean, hSigma = ([] for iList in range(2))
+def create_ratio_hist(num_hist_list, den_hist):
+    ratio_list = []
+    for num_hist in num_hist_list:
+        ratio_hist = den_hist.Clone()
+        ratio_hist.Reset()
+        n_bin_orig = num_hist.GetNbinsX()
+        for ipt in range(ratio_hist.GetNbinsX()+1):
+            pt_cent = ratio_hist.GetBinCenter(ipt)
+            pt_bin = num_hist.FindBin(pt_cent)
+            if 0 < pt_bin <= n_bin_orig:
+                ratio_hist.SetBinContent(ipt, num_hist.GetBinContent(pt_bin))
+                ratio_hist.SetBinError(ipt, num_hist.GetBinError(pt_bin))
+            else:
+                ratio_hist.SetBinContent(ipt, 0.)
+                ratio_hist.SetBinError(ipt, 0.)
+        ratio_hist.Divide(den_hist)
+        ratio_list.append(ratio_hist)
+    return ratio_list
 
-gStyle.SetPadRightMargin(0.035)
-gStyle.SetPadLeftMargin(0.18)
-gStyle.SetPadTopMargin(0.05)
-gStyle.SetTitleSize(0.045,'xy')
-gStyle.SetLabelSize(0.040,'xy')
-gStyle.SetPadTickX(1)
-gStyle.SetPadTickY(1)
-gStyle.SetLegendBorderSize(0)
-gStyle.SetOptStat(0)
+def comp_fit_pars(do_ratio=False): #pylint: disable-msg=too-many-statements
+    inputdir = './'
+    input_files = ['../ML_DsAnalysis/QM_prel/outputs/3050/raw_yields/RawYieldsDs_3050_pt2_36.root',
+                   'outputs/3_24bin_merge/raw_yields/RawYieldsDs_3050_100419cuts.root']
+    input_files_MC = ['../ML_DsAnalysis/QM_prel/outputs/3050/raw_yields/RawYieldsDs_3050_MC_pt2_36.root']
+    colors = [kBlack, kGreen+3, kRed]
+    markers = [kFullCircle, kFullDiamond, kFullSquare]
+    legendnames = ['ML', 'Std', 'ML - MC']
+    suffix = 'compMCStd'
+    min_pt = 2.
+    max_pt = 36.
 
-legSigma = TLegend(0.2,0.78,0.8,0.93)
-legSigma.SetFillStyle(0)
-legSigma.SetBorderSize(0)
-legSigma.SetTextSize(0.04)
+    set_style()
+    hMean, hSigma = [], []
+    input_files = input_files + input_files_MC
 
-legMean = TLegend(0.5,0.73,0.8,0.93)
-legMean.SetFillStyle(0)
-legMean.SetBorderSize(0)
-legMean.SetTextSize(0.04)
+    lineMass = TLine(min_pt, 1.96850, max_pt, 1.96850)
+    lineMass.SetLineWidth(2)
+    lineMass.SetLineColor(kBlack)
+    lineMass.SetLineStyle(9)
 
-lineMass = TLine(3.,1.96850,36,1.96850)
-lineMass.SetLineWidth(2)
-lineMass.SetLineColor(kBlack)
-lineMass.SetLineStyle(9)
+    legSigma = TLegend(0.2, 0.78, 0.8, 0.93)
+    legSigma.SetFillStyle(0)
+    legSigma.SetBorderSize(0)
+    legSigma.SetTextSize(0.04)
 
-for iFile in range(len(inputfilenames)):
-  inputfile = TFile('%s/%s' % (inputdir, inputfilenames[iFile]))
-  hMean.append(inputfile.Get('hRawYieldsMean'))
-  hSigma.append(inputfile.Get('hRawYieldsSigma'))
-  hMean[iFile].SetDirectory(0)
-  hMean[iFile].SetLineColor(colors[iFile])
-  hMean[iFile].SetLineWidth(2)
-  hMean[iFile].SetMarkerColor(colors[iFile])
-  hMean[iFile].SetMarkerStyle(markers[iFile])
-  hSigma[iFile].SetDirectory(0)
-  hSigma[iFile].SetLineColor(colors[iFile])
-  hSigma[iFile].SetLineWidth(2)
-  hSigma[iFile].SetMarkerColor(colors[iFile])
-  hSigma[iFile].SetMarkerStyle(markers[iFile])
-  legMean.AddEntry(hSigma[iFile],legendnames[iFile],'p')
-  legSigma.AddEntry(hSigma[iFile],legendnames[iFile],'p')
+    legMean = TLegend(0.4, 0.73, 0.7, 0.93)
+    legMean.SetFillStyle(0)
+    legMean.SetBorderSize(0)
+    legMean.SetTextSize(0.04)
+    legMean.AddEntry(lineMass, "PDG", 'l')
 
-legMean.AddEntry(lineMass,"PDG",'l')
+    for file_path, color, marker, legend_name in zip(input_files, colors, markers, legendnames):
+        input_file = TFile(f'{inputdir}/{file_path}')
+        histo_mean = input_file.Get('hRawYieldsMean')
+        histo_sigma = input_file.Get('hRawYieldsSigma')
+        histo_mean.SetDirectory(0)
+        histo_mean.SetLineColor(color)
+        histo_mean.SetLineWidth(2)
+        histo_mean.SetMarkerColor(color)
+        histo_mean.SetMarkerStyle(marker)
+        histo_sigma.SetDirectory(0)
+        histo_sigma.SetLineColor(color)
+        histo_sigma.SetLineWidth(2)
+        histo_sigma.SetMarkerColor(color)
+        histo_sigma.SetMarkerStyle(marker)
+        legMean.AddEntry(histo_mean, legend_name, 'p')
+        legSigma.AddEntry(histo_sigma, legend_name, 'p')
+        hMean.append(histo_mean)
+        hSigma.append(histo_sigma)
 
-cMean = TCanvas('cMean','',800,800)
-cMean.DrawFrame(3,1.9641,36,1.9759,';#it{p}_{T} (GeV/#it{c}); peak mean (GeV/#it{c}^{2})')
-lineMass.Draw("same")
-for iFile in range(len(inputfilenames)):
-  hMean[iFile].Draw('same')
-legMean.Draw()
+    if do_ratio:
+        mean_num_list = list(hMean)
+        mean_num_list.pop()
+        mean_den_hist = hMean[-1]
+        mean_ratio_list = create_ratio_hist(mean_num_list, mean_den_hist)
+        cMeanRatio = TCanvas('cMeanRatio', '', 800, 800)
+        cMeanRatio.DrawFrame(min_pt, 0.99, max_pt, 1.01, ';#it{p}_{T} (GeV/#it{c}); peak mean / peak mean MC')
+        lineRatio = TLine(min_pt, 1., max_pt, 1.)
+        lineRatio.SetLineWidth(2)
+        lineRatio.SetLineColor(kBlack)
+        lineRatio.SetLineStyle(9)
+        legMeanRatio = TLegend(0.4, 0.73, 0.7, 0.93)
+        legMeanRatio.SetFillStyle(0)
+        legMeanRatio.SetBorderSize(0)
+        legMeanRatio.SetTextSize(0.04)
+        for hist, color, marker, legend_name in zip(mean_ratio_list, colors[:-1], 
+                                                    markers[:-1], legendnames[:-1]):
+            hist.SetLineColor(color)
+            hist.SetLineWidth(2)
+            hist.SetMarkerColor(color)
+            hist.SetMarkerStyle(marker)
+            hist.Draw('same')
+            legMeanRatio.AddEntry(hist, legend_name, 'p')
+        lineRatio.Draw()
+        legMeanRatio.Draw()
 
-cSigma = TCanvas('cSigma','',800,800)
-cSigma.DrawFrame(3,0.,36,0.025,';#it{p}_{T} (GeV/#it{c}); peak width (GeV/#it{c}^{2})')
-for iFile in range(len(inputfilenames)):
-  hSigma[iFile].Draw('same')
-legSigma.Draw()
+        sigma_num_list = list(hSigma)
+        sigma_num_list.pop()
+        sigma_den_hist = hSigma[-1]
+        sigma_ratio_list = create_ratio_hist(sigma_num_list, sigma_den_hist)
+        cSigmaRatio = TCanvas('cSigmaRatio', '', 800, 800)
+        cSigmaRatio.DrawFrame(min_pt, 0.6, max_pt, 1.8, ';#it{p}_{T} (GeV/#it{c}); peak width / peak width MC')
+        legSigmaRatio = TLegend(0.4, 0.73, 0.7, 0.93)
+        legSigmaRatio.SetFillStyle(0)
+        legSigmaRatio.SetBorderSize(0)
+        legSigmaRatio.SetTextSize(0.04)
+        for hist, color, marker, legend_name in zip(sigma_ratio_list, colors[:-1], 
+                                                    markers[:-1], legendnames[:-1]):
+            hist.SetLineColor(color)
+            hist.SetLineWidth(2)
+            hist.SetMarkerColor(color)
+            hist.SetMarkerStyle(marker)
+            hist.Draw('same')
+            legSigmaRatio.AddEntry(hist, legend_name, 'p')
+        lineRatio.Draw()
+        legSigmaRatio.Draw()
+        cMeanRatio.SaveAs(f'{inputdir}/MeanRatio_{suffix}.pdf')
+        cSigmaRatio.SaveAs(f'{inputdir}/SigmaRatio_{suffix}.pdf')
 
-cMean.SaveAs('%s/Mean_%s.pdf' % (inputdir,outputsuffix))
-cSigma.SaveAs('%s/Sigma_%s.pdf' % (inputdir,outputsuffix))
+    cMean = TCanvas('cMean', '', 800, 800)
+    cMean.DrawFrame(min_pt, 1.9641, max_pt, 1.9759, ';#it{p}_{T} (GeV/#it{c}); peak mean (GeV/#it{c}^{2})')
+    lineMass.Draw("same")
+    for histo_mean in hMean:
+        histo_mean.Draw('same')
+    legMean.Draw()
 
-if six.PY2:
-    raw_input('Press enter to exit')
-elif six.PY3:
+    cSigma = TCanvas('cSigma', '', 800, 800)
+    cSigma.DrawFrame(min_pt, 0., max_pt, 0.025, ';#it{p}_{T} (GeV/#it{c}); peak width (GeV/#it{c}^{2})')
+    for histo_sigma in hSigma:
+        histo_sigma.Draw('same')
+    legSigma.Draw()
+
+    cMean.SaveAs(f'{inputdir}/Mean_{suffix}.pdf')
+    cSigma.SaveAs(f'{inputdir}/Sigma_{suffix}.pdf')
+
+def main():
+    parser = argparse.ArgumentParser(description='Arguments')
+    parser.add_argument("--ratio", help="make ratio to MC", action="store_true")
+    args = parser.parse_args()
+    comp_fit_pars(args.ratio)
     input('Press enter to exit')
+
+main()
