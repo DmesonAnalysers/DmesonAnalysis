f34d0a1d2b04ba66b1887ddf877d04ae6f3b2e1c
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2019-12-08 11:23:00 +0100

    Add docstrings

--- TaskFileLoader.py
+++ TaskFileLoader.py
@@ -4,7 +4,21 @@ python script with helper functions to load objects from task
 
 from ROOT import TFile  # pylint: disable=import-error,no-name-in-module
 
+
 def LoadSparseFromTask(infilename, inputCfg):
+    '''
+    Method to retrieve sparses from output task file
+
+    Inputs
+    ----------
+    - input root file name
+    - config dictionary from yaml file with name of objects in root file
+
+    Returns
+    ----------
+    - list of sparses with reconstructed quantities for all candidates, and prompt, FD D mesons and bkg candidates (only if MC)
+    - list of sparses with generated quantities for prompt and FD D mesons (only if MC)
+    '''
     print('Loading THnSparses from file', infilename)
     infileData = TFile(infilename)
     indirData = infileData.Get(inputCfg['dirname'])
@@ -31,7 +45,21 @@ def LoadSparseFromTask(infilename, inputCfg):
 
     return sparses, sparsesGen
 
+
 def LoadSingleSparseFromTask(infilename, inputCfg, sparsetype='sparsenameBkg'):
+    '''
+    Method to retrieve single sparse from output task file
+
+    Inputs
+    ----------
+    - input root file name
+    - config dictionary from yaml file with name of objects in root file
+    - sparse that should be returned
+
+    Returns
+    ----------
+    - selected sparse from file
+    '''
     print('Loading THnSparse from file', infilename)
     infileData = TFile(infilename)
     indirData = infileData.Get(inputCfg['dirname'])
@@ -42,6 +70,18 @@ def LoadSingleSparseFromTask(infilename, inputCfg, sparsetype='sparsenameBkg'):
 
 
 def LoadNormObjFromTask(infilename, inputCfg):
+    '''
+    Method to retrieve normalisation objects from output task file
+
+    Inputs
+    ----------
+    - input root file name
+    - config dictionary from yaml file with name of objects in root file
+
+    Returns
+    ----------
+    - histo with event info and normalisation counter
+    '''
     print('Loading norm objects from file', infilename)
     infileData = TFile(infilename)
     indirData = infileData.Get(inputCfg['dirname'])
@@ -53,6 +93,18 @@ def LoadNormObjFromTask(infilename, inputCfg):
 
 
 def LoadListFromTask(infilename, inputCfg):
+    '''
+    Method to retrieve list of objects from output task file
+
+    Inputs
+    ----------
+    - input root file name
+    - config dictionary from yaml file with name of objects in root file
+
+    Returns
+    ----------
+    - TList of objects from input file
+    '''
     print('Loading TList from file', infilename)
     infileData = TFile(infilename)
     indirData = infileData.Get(inputCfg['dirname'])
@@ -62,6 +114,18 @@ def LoadListFromTask(infilename, inputCfg):
 
 
 def LoadCutObjFromTask(infilename, inputCfg):
+    '''
+    Method to retrieve D-meson cut object from output task file
+
+    Inputs
+    ----------
+    - input root file name
+    - config dictionary from yaml file with name of objects in root file
+
+    Returns
+    ----------
+    - D-meson cut object
+    '''
     print('Loading cut object from file', infilename)
     infileData = TFile(infilename)
     indirData = infileData.Get(inputCfg['dirname'])
@@ -72,26 +136,21 @@ def LoadCutObjFromTask(infilename, inputCfg):
     return cutobj, cutobjname
 
 
-def LoadPIDTH3(infilename, inputCfg):
-    print('Loading Nsigma distributions from file', infilename)
-    hNsigmaVsPtVsML = {}
-    infileData = TFile(infilename)
-    indirData = infileData.Get(inputCfg['dirname'])
-    inlistData = indirData.Get(inputCfg['listname'])
-    species = ['K', 'Pi']
-    detectors = ['TPC', 'TOF', 'Comb']
-    for det in detectors:
-        hNsigmaVsPtVsML[det] = {}
-        for spe in species:
-            hNsigmaVsPtVsML[det][spe] = {}
-            for iprong in range(3):
-                hNsigmaVsPtVsML[det][spe]['{0}'.format(iprong)] = inlistData.FindObject(
-                    'fHistNsigma{0}{1}Prong{2}'.format(det, spe, iprong))
-
-    return hNsigmaVsPtVsML
-
-
 def LoadPIDSparses(infilename, inputCfg):
+    '''
+    Method to retrieve PID sparses from output task file
+
+    Inputs
+    ----------
+    - input root file name
+    - config dictionary from yaml file with name of objects in root file
+
+    Returns
+    ----------
+    - sparse of NsigmaTPC and NsigmaTOF variables
+    - sparse of NsigmaComb variables
+    - dictionary with variable : sparse-axis number
+    '''
     print('Loading PID THnSparses from file', infilename)
     infileData = TFile(infilename)
     indirData = infileData.Get(inputCfg['dirname'])
@@ -99,14 +158,26 @@ def LoadPIDSparses(infilename, inputCfg):
     sparsePIDNsigma = inlistData.FindObject('fnSparsePID')
     sparsePIDNsigmaComb = inlistData.FindObject('fnSparsePIDcomb')
 
-    #dictionary of sparse axes with detectors, mass hypothesis, daughter number
-    axes = {'TPC': {'Pi':{'0':2, '1':6, '2':10}, 'K':{'0':3, '1':7, '2':11}},
-            'TOF': {'Pi':{'0':4, '1':8, '2':12}, 'K':{'0':5, '1':9, '2':13}},
-            'Comb': {'Pi':{'0':2, '1':4, '2':6}, 'K':{'0':3, '1':5, '2':7}}}
+    # dictionary of sparse axes with detectors, mass hypothesis, daughter number
+    axes = {'TPC': {'Pi': {'0': 2, '1': 6, '2': 10}, 'K': {'0': 3, '1': 7, '2': 11}},
+            'TOF': {'Pi': {'0': 4, '1': 8, '2': 12}, 'K': {'0': 5, '1': 9, '2': 13}},
+            'Comb': {'Pi': {'0': 2, '1': 4, '2': 6}, 'K': {'0': 3, '1': 5, '2': 7}}}
 
     return sparsePIDNsigma, sparsePIDNsigmaComb, axes
 
+
 def LoadSparseFromTaskV2(inputCfg):
+    '''
+    Method to retrieve sparses from D-meson vn output task file
+
+    Inputs
+    ----------
+    - config dictionary from yaml file with name of objects in root file
+
+    Returns
+    ----------
+    - list of sparses with reconstructed quantities for all candidates
+    '''
     infilename = inputCfg['filename']
     print(f'Loading THnSparses from file {infilename}')
     sparses = []
@@ -129,7 +200,21 @@ def LoadSparseFromTaskV2(inputCfg):
 
     return sparses
 
+
 def LoadListFromTaskV2(infilename, dirname, listname):
+    '''
+    Method to retrieve list of objects from D-meson vn output task file
+
+    Inputs
+    ----------
+    - input root file name
+    - name of directory in input root file
+    - name of list in directory in input root file
+
+    Returns
+    ----------
+    - TList of objects from input file
+    '''
     print('Loading TList from file', infilename)
     infileData = TFile(infilename)
     indirData = infileData.Get(dirname)
