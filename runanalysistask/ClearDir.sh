645f5c45c5350a25020513064fa49ff7f50f1ac2
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2020-01-22 15:18:31 +0100

    Add .d and .so files in clean script

--- runanalysistask/ClearDir.sh
+++ runanalysistask/ClearDir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a arrayOfFiles=("outputs_valid" "event_stat.root" "*.xml" "*.jdl" "myAnalysis*" "Improver.root" "EventStat_temp.root" "stdout" "stderr")
+declare -a arrayOfFiles=("outputs_valid" "event_stat.root" "*.xml" "*.jdl" "myAnalysis*" "Improver.root" "EventStat_temp.root" "stdout" "stderr" "*.d" "*.so")
 
 for FILE in "${arrayOfFiles[@]}"
 do
