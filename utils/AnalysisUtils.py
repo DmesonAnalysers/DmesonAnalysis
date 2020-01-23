57672d83fe10ecb7d9ec88c15d4cbdbf9a1db2aa
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2020-01-23 18:43:35 +0100

    Add functions for D-meson MC signal fits

--- utils/AnalysisUtils.py
+++ utils/AnalysisUtils.py
@@ -3,7 +3,7 @@ Script with miscellanea utils methods for the analysis
 '''
 
 import numpy as np
-from ROOT import TH1F # pylint: disable=import-error,no-name-in-module
+from ROOT import TH1F, TMath # pylint: disable=import-error,no-name-in-module
 
 def ComputeEfficiency(recoCounts, genCounts, recoCountsError, genCountsError):
     '''
@@ -103,3 +103,85 @@ def GetPromptFDYieldsAnalyticMinimisation(effPromptList, effFDList, rawYieldList
         mCorrYieldOld = np.copy(mCorrYield)
 
     return mCorrYield, mCovariance
+
+
+def SingleGaus(x, par):
+    """
+    Gaussian function
+
+    Parameters
+    ----------
+
+    - x: function variable
+    - par: function parameters
+        par[0]: normalisation
+        par[1]: mean
+        par[2]: sigma
+    """
+    return par[0]*TMath.Gaus(x[0], par[1], par[2], True)
+
+
+def DoubleGaus(x, par):
+    """
+    Sum of two Gaussian functions with same mean and different sigma
+
+    Parameters
+    ----------
+
+    - x: function variable
+    - par: function parameters
+        par[0]: normalisation
+        par[1]: mean
+        par[2]: first sigma
+        par[3]: second sigma
+        par[4]: fraction of integral in second Gaussian
+    """
+    firstGaus = TMath.Gaus(x[0], par[1], par[2], True)
+    secondGaus = TMath.Gaus(x[0], par[1], par[3], True)
+    return par[0] * ((1-par[4])*firstGaus + par[4]*secondGaus)
+
+
+def DoublePeakSingleGaus(x, par):
+    """
+    Sum of two Gaussian functions with different mean and sigma
+
+    Parameters
+    ----------
+
+    - x: function variable
+    - par: function parameters
+        par[0]: normalisation first peak
+        par[1]: mean first peak
+        par[2]: sigma first peak
+        par[3]: normalisation second peak
+        par[4]: mean second peak
+        par[5]: sigma second peak
+    """
+    firstGaus = par[0]*TMath.Gaus(x[0], par[1], par[2], True)
+    secondGaus = par[3]*TMath.Gaus(x[0], par[4], par[5], True)
+    return firstGaus + secondGaus
+
+
+def DoublePeakDoubleGaus(x, par):
+    """
+    Sum of a double Gaussian function and a single Gaussian function
+
+    Parameters
+    ----------
+
+    - x: function variable
+    - par: function parameters
+        par[0]: normalisation first peak
+        par[1]: mean first peak
+        par[2]: first sigma first peak
+        par[3]: second sigma first peak
+        par[4]: fraction of integral in second Gaussian first peak
+        par[5]: normalisation second peak
+        par[6]: mean second peak
+        par[7]: sigma second peak
+    """
+    firstGaus = TMath.Gaus(x[0], par[1], par[2], True)
+    secondGaus = TMath.Gaus(x[0], par[1], par[3], True)
+    thirdGaus = par[5]*TMath.Gaus(x[0], par[6], par[7], True)
+    return par[0] * ((1-par[4])*firstGaus + par[4]*secondGaus) + thirdGaus
+    
