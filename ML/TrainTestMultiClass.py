b2eb51e33d27131d86321fc65c94d0e2c899daa3
Fabio Catalano <fabio.catalano@cern.ch>
2020-01-27 16:28:07 +0100

    Use f-strings

--- ML/TrainTestMultiClass.py
+++ ML/TrainTestMultiClass.py
@@ -1,3 +1,8 @@
+'''
+python script to run basic training and application using the hipe4ml package
+run: python TrainTestMulticlass.py cfgFileName.yml
+'''
+
 import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
@@ -26,44 +31,42 @@ for (PtMin, PtMax) in zip(PtMins, PtMaxs):
 
     print('\nStarting ML analysis')
 
-    DataDfPtSel = DataDf.query('{0} < pt_cand < {1}'.format(PtMin, PtMax))
+    DataDfPtSel = DataDf.query(f'{PtMin} < pt_cand < {PtMax}')
     BkgDfPtSel = DataDfPtSel.query('inv_mass < 1.82 or 1.92 < inv_mass < 2.00')
-    PromptDfPtSel = PromptDf.query('{0} < pt_cand < {1}'.format(PtMin, PtMax))
-    FDDfPtSel = FDDf.query('{0} < pt_cand < {1}'.format(PtMin, PtMax))
+    PromptDfPtSel = PromptDf.query(f'{PtMin} < pt_cand < {PtMax}')
+    FDDfPtSel = FDDf.query(f'{PtMin} < pt_cand < {PtMax}')
 
     nPrompt = len(PromptDfPtSel)
     nFD = len(FDDfPtSel)
     nBkg = len(BkgDfPtSel)
-    print('\nNumber of available candidates in {0} < pT < {1} GeV/c:'.format(PtMin, PtMax))
+    print(f'\nNumber of available candidates in {PtMin} < pT < {PtMax} GeV/c:')
     print('Prompt:', nPrompt)
     print('FD:', nFD)
     print('Bkg:', nBkg)
 
     nCandToKeep = min([nPrompt, nFD, nBkg])
-    print('\nKeep same number of prompt, FD, and background (minimum) for training and testing (50-50):', \
-        nCandToKeep)
+    print('\nKeep same number of prompt, FD, and background (minimum) for training and testing (50-50):',
+          nCandToKeep)
 
     print('Fraction of real data candidates used for ML: ', nCandToKeep / nBkg)
 
     if nPrompt > nFD:
-        print('Remaining prompt candidates ({0}) will be used for the efficiency together with test set'.format(\
-            nPrompt-nCandToKeep))
+        print(f'Remaining prompt candidates ({nPrompt - nCandToKeep})' \
+              'will be used for the efficiency together with test set')
     elif nFD > nPrompt:
-        print('Remaining FD candidates ({0}) will be used for the efficiency together with test set\n'.format(\
-            nFD-nCandToKeep))
+        print(f'Remaining FD candidates ({nFD - nCandToKeep}) will be used for the efficiency together with test set\n')
 
-    TotDfPtSel = pd.concat([BkgDfPtSel.iloc[:nCandToKeep].copy(), PromptDfPtSel.iloc[:nCandToKeep].copy(), \
-        FDDfPtSel.iloc[:nCandToKeep].copy()], sort=True)
+    TotDfPtSel = pd.concat([BkgDfPtSel.iloc[:nCandToKeep].copy(), PromptDfPtSel.iloc[:nCandToKeep].copy(),
+                           FDDfPtSel.iloc[:nCandToKeep].copy()], sort=True)
 
-    LabelsArray = [0 for iCand in range(nCandToKeep)] + \
-        [1 for iCand in range(nCandToKeep)] + [2 for iCand in range(nCandToKeep)]
+    LabelsArray = [0 for _ in range(nCandToKeep)] + [1 for _ in range(nCandToKeep)] + [2 for _ in range(nCandToKeep)]
 
     TrainSet, TestSet, yTrain, yTest = train_test_split(TotDfPtSel, LabelsArray, test_size=0.5, random_state=42)
     TrainTestData = [TrainSet, yTrain, TestSet, yTest]
 
     CandTypeFlags = pd.Series(yTest)
-    PromptDfPtSelForEff = pd.concat([PromptDfPtSel.iloc[nCandToKeep:].copy(), TestSet[CandTypeFlags.values == 1]], \
-        sort=False)
+    PromptDfPtSelForEff = pd.concat([PromptDfPtSel.iloc[nCandToKeep:].copy(), TestSet[CandTypeFlags.values == 1]],
+                                    sort=False)
     FDDfPtSelForEff = pd.concat([FDDfPtSel.iloc[nCandToKeep:].copy(), TestSet[CandTypeFlags.values == 2]], sort=False)
 
     #remove pT and inv-mass from training columns --> training columns to be set in a config yaml file
@@ -98,27 +101,24 @@ for (PtMin, PtMax) in zip(PtMins, PtMaxs):
     yPredPromptEff = ModelHandl.predict(PromptDfPtSelForEff)
     PromptDfPtSelForEff = PromptDfPtSelForEff.loc[:, ['inv_mass', 'pt_cand']]
     for Pred, Lab in enumerate(OutputLabels):
-        PromptDfPtSelForEff['ML_output_{0}'.format(Lab)] = yPredPromptEff[:, Pred]
-    PromptDfPtSelForEff.to_parquet('{0}/Prompt_Dpluspp5TeV_pT_{1}_{2}_ModelApplied.parquet.gzip'.format(\
-        OutPutDir, PtMin, PtMax))
+        PromptDfPtSelForEff[f'ML_output_{Lab}'] = yPredPromptEff[:, Pred]
+    PromptDfPtSelForEff.to_parquet(f'{OutPutDir}/Prompt_Dpluspp5TeV_pT_{PtMin}_{PtMax}_ModelApplied.parquet.gzip')
     print('Applying ML model to prompt dataframe: Done!')
 
     print('Applying ML model to FD dataframe: ...', end='\r')
     yPredFDEff = ModelHandl.predict(FDDfPtSelForEff)
     FDDfPtSelForEff = FDDfPtSelForEff.loc[:, ['inv_mass', 'pt_cand']]
     for Pred, Lab in enumerate(OutputLabels):
-        FDDfPtSelForEff['ML_output_{0}'.format(Lab)] = yPredFDEff[:, Pred]
-    FDDfPtSelForEff.to_parquet('{0}/FD_Dpluspp5TeV_pT_{1}_{2}_ModelApplied.parquet.gzip'.format(\
-        OutPutDir, PtMin, PtMax))
+        FDDfPtSelForEff[f'ML_output_{Lab}'] = yPredFDEff[:, Pred]
+    FDDfPtSelForEff.to_parquet(f'{OutPutDir}/FD_Dpluspp5TeV_pT_{PtMin}_{PtMax}_ModelApplied.parquet.gzip')
     print('Applying ML model to FD dataframe: Done!')
 
     print('Applying ML model to data dataframe: ...', end='\r')
     yPredData = ModelHandl.predict(DataDfPtSel)
     DataDfPtSel = DataDfPtSel.loc[:, ['inv_mass', 'pt_cand']]
     for Pred, Lab in enumerate(OutputLabels):
-        DataDfPtSel['ML_output_{0}'.format(Lab)] = yPredData[:, Pred]
-    DataDfPtSel.to_parquet('{0}/Data_Dpluspp5TeV_pT_{1}_{2}_ModelApplied.parquet.gzip'.format(\
-        OutPutDir, PtMin, PtMax))
+        DataDfPtSel[f'ML_output_{Lab}'] = yPredData[:, Pred]
+    DataDfPtSel.to_parquet(f'{OutPutDir}/Data_Dpluspp5TeV_pT_{PtMin}_{PtMax}_ModelApplied.parquet.gzip')
     print('Applying ML model to data dataframe: Done!')
 
 
@@ -129,10 +129,10 @@ for (PtMin, PtMax) in zip(PtMins, PtMaxs):
     VarsToDraw = list(TotDfPtSel.columns)
 
     #_____________________________________________
-    FeatDistrFig = plot_utils.plot_distr(\
-        [BkgDfPtSel, PromptDfPtSel, FDDfPtSel], VarsToDraw, (12, 7), 100, True, LegLabels)
+    FeatDistrFig = plot_utils.plot_distr([BkgDfPtSel, PromptDfPtSel, FDDfPtSel], VarsToDraw, (12, 7), 
+                                         100, True, LegLabels)
     plt.subplots_adjust(left=0.06, bottom=0.06, right=0.99, top=0.96, hspace=0.55, wspace=0.55)
-    plt.savefig('{0}/DistributionsAll_pT_{1}_{2}.pdf'.format(OutPutDir, PtMin, PtMax))
+    plt.savefig(f'{OutPutDir}/DistributionsAll_pT_{PtMin}_{PtMax}.pdf')
     plt.close('all')
 
     #_____________________________________________
@@ -140,31 +140,31 @@ for (PtMin, PtMax) in zip(PtMins, PtMaxs):
     for Fig, Lab in zip(CorrMatrixFig, OutputLabels):
         plt.figure(Fig.number)
         plt.subplots_adjust(left=0.2, bottom=0.25, right=0.95, top=0.9)
-        Fig.savefig('{0}/CorrMatrix{1}_pT_{2}_{3}.pdf'.format(OutPutDir, Lab, PtMin, PtMax))
+        Fig.savefig(f'{OutPutDir}/CorrMatrix{Lab}_pT_{PtMin}_{PtMax}.pdf')
 
     #_____________________________________________
     plt.rcParams["figure.figsize"] = (10, 7)
     MLOutputFig = plot_utils.plot_output_train_test(ModelHandl, TrainTestData, 80, True, LegLabels)
     for Fig, Lab in zip(MLOutputFig, OutputLabels):
-        Fig.savefig('{0}/MLOutputDistr{1}_pT_{2}_{3}.pdf'.format(OutPutDir, Lab, PtMin, PtMax))
+        Fig.savefig(f'{OutPutDir}/MLOutputDistr{Lab}_pT_{PtMin}_{PtMax}.pdf')
 
     #_____________________________________________
     plt.rcParams["figure.figsize"] = (8, 7)
     ROCCurveFig = plot_utils.plot_roc(TrainTestData[3], yPredTest, LegLabels)
-    ROCCurveFig.savefig('{0}/ROCCurveAll_pT_{1}_{2}.pdf'.format(OutPutDir, PtMin, PtMax))
+    ROCCurveFig.savefig(f'{OutPutDir}/ROCCurveAll_pT_{PtMin}_{PtMax}.pdf')
 
     #_____________________________________________
     PrecisionRecallFig = plot_utils.plot_precision_recall(TrainTestData[3], yPredTest, LegLabels)
-    PrecisionRecallFig.savefig('{0}/PrecisionRecallAll_pT_{1}_{2}.pdf'.format(OutPutDir, PtMin, PtMax))
+    PrecisionRecallFig.savefig(f'{OutPutDir}/PrecisionRecallAll_pT_{PtMin}_{PtMax}.pdf')
 
     #_____________________________________________
     plt.rcParams["figure.figsize"] = (12, 7)
     FeaturesImportanceFig = plot_utils.plot_feature_imp(TrainSet[TrainCols], yTrain, ModelHandl)
     for iFig, Fig in enumerate(FeaturesImportanceFig):
         if iFig < 3:
-            Fig.savefig('{0}/FeatureImportance{1}_pT_{2}_{3}.pdf'.format(OutPutDir, OutputLabels[iFig], PtMin, PtMax))
+            Fig.savefig(f'{OutPutDir}/FeatureImportance{OutputLabels[iFig]}_pT_{PtMin}_{PtMax}.pdf')
         else:
-            Fig.savefig('{0}/FeatureImportanceAll_pT_{1}_{2}.pdf'.format(OutPutDir, PtMin, PtMax))
+            Fig.savefig(f'{OutPutDir}/FeatureImportanceAll_pT_{PtMin}_{PtMax}.pdf')
 
     # delete dataframes to release memory
     del DataDfPtSel, BkgDfPtSel, PromptDfPtSel, FDDfPtSel, TrainSet, TestSet, yTrain, yTest
