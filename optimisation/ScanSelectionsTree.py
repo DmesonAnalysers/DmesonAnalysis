238d44132fee55f8082e7dd786a92bc975143a0e
StefanoP <59452587+stefanopolitano@users.noreply.github.com>
2020-03-11 18:10:33 +0100

    Raa from .txt (#41)
    * Add possibility to read Raa from .txt and initial implementation  on DCA plots

--- optimisation/ScanSelectionsTree.py
+++ optimisation/ScanSelectionsTree.py
@@ -10,14 +10,16 @@ import itertools
 import numpy as np
 import yaml
 from root_numpy import fill_hist
-from ROOT import TFile, TH1F, TH2F, TCanvas, TNtuple, TDirectoryFile  # pylint: disable=import-error,no-name-in-module
-from ROOT import gROOT, kRainBow, kBlack, kFullCircle  # pylint: disable=import-error,no-name-in-module
+from ROOT import TFile, TH1F, TH2F, TCanvas, TNtuple, TDirectoryFile, TAxis, TLegend  # pylint: disable=import-error,no-name-in-module
+from ROOT import gROOT, kRainBow, kBlack, kBlue, kRed, kFullCircle  # pylint: disable=import-error,no-name-in-module
 sys.path.append('..')
 # pylint: disable=wrong-import-position,import-error,no-name-in-module
 from utils.AnalysisUtils import ComputeEfficiency, GetPromptFDFractionFc, GetExpectedBkgFromSideBands, \
     GetExpectedBkgFromMC, GetExpectedSignal
 from utils.StyleFormatter import SetGlobalStyle, SetObjectStyle
 from utils.DfUtils import LoadDfFromRootOrParquet
+from utils.ReadModel import InterpolateModel, ReadTAMU, ReadPHSD, \
+    ReadMCatsHQ, ReadCatania
 
 parser = argparse.ArgumentParser(description='Arguments to pass')
 parser.add_argument('cfgFileName', metavar='text', default='cfgFileName.yml',
@@ -93,14 +95,51 @@ hCrossSecFD = inFileCrossSec.Get(inputCfg['predictions']['crosssec']['histonames
 
 # load RAA
 # TODO: add possibility to pass prediction from file instead of fixed value
-RaaPrompt = inputCfg['predictions']['Raa']['prompt']
-if not isinstance(RaaPrompt, float) and not isinstance(RaaPrompt, int):
-    print('ERROR: currently only fixed values of RAA are supported. Exit')
-    exit()
-RaaFD = inputCfg['predictions']['Raa']['feeddown']
-if not isinstance(RaaFD, float) and not isinstance(RaaFD, int):
-    print('ERROR: currently only fixed values of RAA are supported. Exit')
-    exit()
+RaaPrompt_config = inputCfg['predictions']['Raa']['prompt']
+if not isinstance(RaaPrompt_config, float) and not isinstance(RaaPrompt_config, int):
+    if not isinstance(RaaPrompt_config, str):
+        print('ERROR: RAA must be at least a string or a number. Exit')
+        exit()
+    else:
+        Raa_model_name = inputCfg['predictions']['Raa']['model']
+        if Raa_model_name not in ['phsd', 'Catania', 'tamu', 'MCatsHQ']:
+            print('ERROR: wrong model name, please check the list of avaliable models. Exit')
+            exit()
+        else:
+            if Raa_model_name == 'phsd':
+                RaaPromptSpline, _ = ReadPHSD(RaaPrompt_config)
+            elif Raa_model_name == 'Catania':
+                RaaPromptSpline, _ = ReadCatania(RaaPrompt_config)
+            elif Raa_model_name == 'MCatsHQ':
+                RaaFDSpline, _ = ReadMCatsHQ(RaaFD_config)
+            elif Raa_model_name == 'tamu':
+                RaaPromptSpline, _ = ReadTAMU(RaaPrompt_config)
+
+else:
+    RaaPrompt = RaaPrompt_config
+
+RaaFD_config = inputCfg['predictions']['Raa']['feeddown']
+if not isinstance(RaaFD_config, float) and not isinstance(RaaFD_config, int):
+    if not isinstance(RaaFD_config, str):
+        print('ERROR: RAA must be at least a string or a number. Exit')
+        exit()
+    else:
+        Raa_model_name = inputCfg['predictions']['Raa']['model']
+        if Raa_model_name not in ['phsd', 'Catania', 'tamu', 'MCatsHQ']:
+            print('ERROR: wrong model name, please check the list of avaliable models. Exit')
+            exit()
+        else:
+            if Raa_model_name == 'phsd':
+                RaaFDSpline, _ = ReadPHSD(RaaFD_config)
+            elif Raa_model_name == 'Catania':
+                RaaFDSpline, _ = ReadCatania(RaaFD_config)
+            elif Raa_model_name == 'MCatsHQ':
+                RaaFDSpline, _ = ReadMCatsHQ(RaaFD_config)
+            elif Raa_model_name == 'tamu':
+                RaaFDSpline, _ = ReadTAMU(RaaFD_config)
+
+else:
+    RaaFD = RaaFD_config
 
 # load constant terms
 nExpEv = inputCfg['nExpectedEvents']
@@ -127,6 +166,7 @@ outDirPlotsPt = []
 
 estNames = {'Signif':'expected significance', 'SoverB':'S/B', 'EffAccPrompt':'(Acc#times#font[152]{e})_{prompt}',
             'EffAccFD':'(Acc#times#font[152]{e})_{FD}', 'fPrompt':'#it{f}_{ prompt}^{ fc}', 'fFD':'#it{f}_{ FD}^{ fc}'}
+
 varsName4Tuple = ':'.join(cutVars) + ':PtMin:PtMax:S:B:' + ':'.join(estNames.keys())
 tSignif = TNtuple('tSignif', 'tSignif', varsName4Tuple)
 
@@ -140,7 +180,6 @@ SetGlobalStyle(padleftmargin=0.2, padrightmargin=0.2, padbottommargin=0.15,
 
 cSignifVsRest, hSignifVsRest, cEstimVsCut, hEstimVsCut = [], [], [], []
 for iPt, (ptMin, ptMax) in enumerate(zip(ptMins, ptMaxs)):
-
     if not inputCfg['infiles']['background']['isMC']:
         outDirFitSB.cd()
         outDirFitSBPt.append(TDirectoryFile(f'pT{ptMin}-{ptMax}', f'pT{ptMin}-{ptMax}'))
@@ -148,15 +187,15 @@ for iPt, (ptMin, ptMax) in enumerate(zip(ptMins, ptMaxs)):
     outDirPlots.cd()
     outDirPlotsPt.append(TDirectoryFile(f'pT{ptMin}-{ptMax}', f'pT{ptMin}-{ptMax}'))
     outDirPlotsPt[iPt].Write()
-
     dfPromptPt = dfPrompt.query(f'{ptMin} < pt_cand < {ptMax}')
     dfFDPt = dfFD.query(f'{ptMin} < pt_cand < {ptMax}')
     dfBkgPt = dfBkg.query(f'{ptMin} < pt_cand < {ptMax}')
-
+    ptCent = (ptMax+ptMin)/2.
+    RaaPrompt = RaaPromptSpline['yCent'](ptCent)
+    RaaFD = RaaFDSpline['yCent'](ptCent)
     # denominator for efficiency
     nTotPrompt = len(dfPromptPt)
     nTotFD = len(dfFDPt)
-
     # preselection efficiency (if input provided)
     if inputCfg['infiles']['preseleff']['filename']:
         ptBinPreselEff = hPreselEffPrompt.GetXaxis().FindBin(ptMin*1.0001)
@@ -165,24 +204,20 @@ for iPt, (ptMin, ptMax) in enumerate(zip(ptMins, ptMaxs)):
     else:
         preselEffPrompt = 1.
         preselEffFD = 1.
-
     # acceptance
     ptBinAccMin = hPtGenAcc.GetXaxis().FindBin(ptMin*1.0001)
     ptBinAccMax = hPtGenAcc.GetXaxis().FindBin(ptMax*0.9999)
     numAcc = hPtGenAcc.Integral(ptBinAccMin, ptBinAccMax)
     denAcc = hPtGenLimAcc.Integral(ptBinAccMin, ptBinAccMax)
     acc, accUnc = ComputeEfficiency(numAcc, denAcc, np.sqrt(numAcc), np.sqrt(denAcc))
-
     # cross section from theory
     ptBinCrossSecMin = hCrossSecPrompt.GetXaxis().FindBin(ptMin*1.0001)
     ptBinCrossSecMax = hCrossSecPrompt.GetXaxis().FindBin(ptMax*0.9999)
     crossSecPrompt = hCrossSecPrompt.Integral(ptBinCrossSecMin, ptBinCrossSecMax, 'width') / (ptMax - ptMin)
     crossSecFD = hCrossSecFD.Integral(ptBinCrossSecMin, ptBinCrossSecMax, 'width') / (ptMax - ptMin)
-
     # output histos
     hSignifVsRest.append(dict())
     hEstimVsCut.append(dict())
-
     if len(varNames) == 1:
         for est in estNames:
             minVar = cutVars[varNames[0]]['min'] - cutVars[varNames[0]]['step'] / 2
@@ -202,8 +237,8 @@ for iPt, (ptMin, ptMax) in enumerate(zip(ptMins, ptMaxs)):
             hEstimVsCut[iPt][est] = TH2F(f'h{est}VsCut_pT{ptMin}-{ptMax}',
                                          f';{varNames[0]};{varNames[1]};{estNames[est]}',
                                          nBinsVar0, minVar0, maxVar0, nBinsVar1, minVar1, maxVar1)
-
     startTime = time.time()
+
     for iSet, cutSet in enumerate(itertools.product(*cutRanges)):
         selToApply = ''
         for iCut, (cut, upperLower, varName) in enumerate(zip(cutSet, upperLowerCuts, varNames)):
@@ -211,38 +246,33 @@ for iPt, (ptMin, ptMax) in enumerate(zip(ptMins, ptMaxs)):
                 selToApply = f'{varName}{upperLower}{cut}'
             else:
                 selToApply += f' & {varName}{upperLower}{cut}'
-
         if (iSet+1) % 100 == 0:
             print(f'Time elapsed to test up to cut set number {iSet+1}: {time.time()-startTime:.2f}s', end='\r')
-
         dfPromptPtSel = dfPromptPt.query(selToApply)
         dfFDPtSel = dfFDPt.query(selToApply)
         dfBkgPtSel = dfBkgPt.query(selToApply)
-
         effPrompt, effPromptUnc = ComputeEfficiency(len(dfPromptPtSel), nTotPrompt,
                                                     np.sqrt(len(dfPromptPtSel)), np.sqrt(nTotPrompt))
         effFD, effFDUnc = ComputeEfficiency(len(dfFDPtSel), nTotFD, np.sqrt(len(dfFDPtSel)), np.sqrt(nTotFD))
-
         effTimesAccPrompt = effPrompt * preselEffPrompt * acc
         effTimesAccFD = effFD * preselEffFD * acc
-
         fPrompt, fFD = GetPromptFDFractionFc(
             effTimesAccPrompt, effTimesAccFD, crossSecPrompt, crossSecFD, RaaPrompt, RaaFD)
-
         hMassBkg = TH1F(f'hMassBkg_pT{ptMin}-{ptMax}_cutSet{iSet}', ';#it{M} (GeV/#it{c});Counts',
                         400, min(dfBkgPtSel['inv_mass']), max(dfBkgPtSel['inv_mass']))
         hMassSignal = TH1F(f'hMassSignal_pT{ptMin}-{ptMax}_cutSet{iSet}', ';#it{M} (GeV/#it{c});Counts', 400, min(
             dfPromptPtSel['inv_mass']), max(dfPromptPtSel['inv_mass']))
+
         fill_hist(hMassBkg, dfBkgPtSel['inv_mass'].values)
         fill_hist(hMassSignal, list(dfPromptPtSel['inv_mass'].values)+list(dfFDPtSel['inv_mass'].values))
 
-        if dfSecPeakPrompt and dfSecPeakFD:
+        # SecPeak
+        if dfSecPeakPrompt and dfSecPeakFD: 
             hMassSeaPeak = TH1F(f'hMassSignal_pT{ptMin}-{ptMax}_cutSet{iSet}', ';#it{M} (GeV/#it{c});Counts', 400, min(
                 dfSecPeakPrompt['inv_mass']), max(dfSecPeakPrompt['inv_mass']))
             fill_hist(hMassSeaPeak, list(dfSecPeakPrompt['inv_mass'].values)+list(dfSecPeakFD['inv_mass'].values))
         else:
             hMassSeaPeak = None
-
         # expected signal
         expSignal = GetExpectedSignal(crossSecPrompt, ptMax-ptMin, 1., effTimesAccPrompt,
                                       fPrompt[0], 1., 1., nExpEv, sigmaMB, Taa, RaaPrompt)
@@ -258,28 +288,22 @@ for iPt, (ptMin, ptMax) in enumerate(zip(ptMins, ptMaxs)):
                 expBkg, hMassSB = GetExpectedBkgFromSideBands(hMassBkg, 'pol2', 4, hMassSignal)
             outDirFitSBPt[iPt].cd()
             hMassSB.Write()
-
         expBkg *= nExpEv / inputCfg['infiles']['background']['nEvents'] / \
             inputCfg['infiles']['background']['fractiontokeep']
-
         # S/B and significance
         if expBkg > 0:
             expSoverB = expSignal / expBkg
         else:
             expSoverB = -1.
-
         if expSignal + expBkg > 0:
             expSignif = expSignal / (np.sqrt(expSignal + expBkg))
         else:
             expSignif = -1.
-
         tupleForNtuple = cutSet + (ptMin, ptMax, expSignal, expBkg, expSignif,
                                    expSoverB, effTimesAccPrompt, effTimesAccFD, fPrompt[0], fFD[0])
         tSignif.Fill(np.array(tupleForNtuple, 'f'))
-
         estValues = {'Signif': expSignif, 'SoverB': expSoverB, 'EffAccPrompt': effTimesAccPrompt,
                      'EffAccFD': effTimesAccFD, 'fPrompt': fPrompt[0], 'fFD': fFD[0]}
-
         if len(varNames) == 1:
             binVar = hEstimVsCut[iPt]['Signif'].GetXaxis().FindBin(cutSet[0])
             for est in estValues:
@@ -289,13 +313,10 @@ for iPt, (ptMin, ptMax) in enumerate(zip(ptMins, ptMaxs)):
             binVar1 = hEstimVsCut[iPt]['Signif'].GetYaxis().FindBin(cutSet[1])
             for est in estValues:
                 hEstimVsCut[iPt][est].SetBinContent(binVar0, binVar1, estValues[est])
-
     print(f'Time elapsed to test cut sets for pT bin {ptMin}-{ptMax}: {time.time()-startTime:.2f}')
-
     # plots
     cSignifVsRest.append(TCanvas(f'cSignifVsRest_pT{ptMin}-{ptMax}', '', 1920, 1080))
     cSignifVsRest[iPt].Divide(3, 2)
-
     for iPad, est in enumerate(estNames):
         if est != 'Signif':
             hFrame = cSignifVsRest[iPt].cd(iPad).DrawFrame(tSignif.GetMinimum(est)*0.8,
@@ -315,10 +336,8 @@ for iPt, (ptMin, ptMax) in enumerate(zip(ptMins, ptMaxs)):
             cSignifVsRest[iPt].Modified()
             outDirPlotsPt[iPt].cd()
             hSignifVsRest[iPt][est].Write()
-
     outDirPlotsPt[iPt].cd()
     cSignifVsRest[iPt].Write()
-
     if 1 <= len(varNames) <= 2:
         if len(varNames) == 1:
             cEstimVsCut.append(TCanvas(f'cEstimVsCut_pT{ptMin}-{ptMax}', '', 1920, 1080))
