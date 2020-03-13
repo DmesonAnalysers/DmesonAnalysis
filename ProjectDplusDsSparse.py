7af778dafdd291504d8006f88a4e4f4f2710e0cb
Fabio Catalano <fabio.catalano@cern.ch>
2020-03-13 18:25:22 +0100

    Cleaning + Various stuff (#47)
    * Minor cleaning
    
    * Better organization of config files
    
    * Clean project from sparse
    
    * Add merging of neightbour bins
    
    * Update configs for Ds in pp

--- ProjectDplusDsSparse.py
+++ ProjectDplusDsSparse.py
@@ -59,14 +59,16 @@ fd_gen_list_secpeak = []
 outfile = TFile(args.outFileName, 'recreate')
 
 for iPt, (ptMin, ptMax) in enumerate(zip(cutVars['Pt']['min'], cutVars['Pt']['max'])):
-    print("Projecting distributions for %0.1f < pT < %0.1f GeV/c" % (ptMin, ptMax))
+    print(f'Projecting distributions for {ptMin:.1f} < pT < {ptMax:.1f} GeV/c')
+    ptLowLabel = ptMin * 10
+    ptHighLabel = ptMax * 10
     for iVar in cutVars:
         if iVar == 'InvMass':
             continue
         binMin = sparseReco['RecoAll'].GetAxis(
-            cutVars[iVar]['axisnum']).FindBin(cutVars[iVar]['min'][iPt]*1.0001)
+            cutVars[iVar]['axisnum']).FindBin(cutVars[iVar]['min'][iPt] * 1.0001)
         binMax = sparseReco['RecoAll'].GetAxis(
-            cutVars[iVar]['axisnum']).FindBin(cutVars[iVar]['max'][iPt]*0.9999)
+            cutVars[iVar]['axisnum']).FindBin(cutVars[iVar]['max'][iPt] * 0.9999)
         sparseReco['RecoAll'].GetAxis(cutVars[iVar]['axisnum']).SetRange(binMin, binMax)
         if isMC:
             sparseReco['RecoPrompt'].GetAxis(cutVars[iVar]['axisnum']).SetRange(binMin, binMax)
@@ -76,27 +78,28 @@ for iPt, (ptMin, ptMax) in enumerate(zip(cutVars['Pt']['min'], cutVars['Pt']['ma
                 sparseReco['RecoSecPeakFD'].GetAxis(cutVars[iVar]['axisnum']).SetRange(binMin, binMax)
 
     for iVar in ('InvMass', 'Pt'):
+        varName = cutVars[iVar]['name']
         hVar = sparseReco['RecoAll'].Projection(cutVars[iVar]['axisnum'])
-        hVar.SetName('h%s_%0.f_%0.f' % (cutVars[iVar]['name'], ptMin*10, ptMax*10))
+        hVar.SetName(f'h{varName}_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
         outfile.cd()
         all_dict[iVar].append(hVar)
         hVar.Write()
         if isMC:
             hVarPrompt = sparseReco['RecoPrompt'].Projection(cutVars[iVar]['axisnum'])
-            hVarPrompt.SetName('hPrompt%s_%0.f_%0.f' % (cutVars[iVar]['name'], ptMin*10, ptMax*10))
+            hVarPrompt.SetName(f'hPrompt%{varName}_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
             prompt_dict[iVar].append(hVarPrompt)
             hVarPrompt.Write()
             hVarFD = sparseReco['RecoFD'].Projection(cutVars[iVar]['axisnum'])
-            hVarFD.SetName('hFD%s_%0.f_%0.f' % (cutVars[iVar]['name'], ptMin*10, ptMax*10))
+            hVarFD.SetName(f'hFD{varName}_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
             fd_dict[iVar].append(hVarFD)
             hVarFD.Write()
             if enableSecPeak:
                 hVarPromptSecPeak = sparseReco['RecoSecPeakPrompt'].Projection(cutVars[iVar]['axisnum'])
-                hVarPromptSecPeak.SetName('hPromptSecPeak%s_%0.f_%0.f' % (cutVars[iVar]['name'], ptMin*10, ptMax*10))
+                hVarPromptSecPeak.SetName(f'hPromptSecPeak{varName}_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
                 prompt_dict_secpeak[iVar].append(hVarPromptSecPeak)
                 hVarPromptSecPeak.Write()
                 hVarFDSecPeak = sparseReco['RecoSecPeakFD'].Projection(cutVars[iVar]['axisnum'])
-                hVarFDSecPeak.SetName('hFDSecPeak%s_%0.f_%0.f' % (cutVars[iVar]['name'], ptMin*10, ptMax*10))
+                hVarFDSecPeak.SetName(f'hFDSecPeak{varName}_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
                 fd_dict_secpeak[iVar].append(hVarFDSecPeak)
                 hVarFDSecPeak.Write()
     if isMC:
@@ -105,22 +108,22 @@ for iPt, (ptMin, ptMax) in enumerate(zip(cutVars['Pt']['min'], cutVars['Pt']['ma
         sparseGen['GenPrompt'].GetAxis(0).SetRange(binGenMin, binGenMax)
         sparseGen['GenFD'].GetAxis(0).SetRange(binGenMin, binGenMax)
         hGenPtPrompt = sparseGen['GenPrompt'].Projection(0)
-        hGenPtPrompt.SetName('hPromptGenPt_%0.f_%0.f' % (ptMin*10, ptMax*10))
+        hGenPtPrompt.SetName(f'hPromptGenPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
         prompt_gen_list.append(hGenPtPrompt)
         hGenPtPrompt.Write()
         hGenPtFD = sparseGen['GenFD'].Projection(0)
-        hGenPtFD.SetName('hFDGenPt_%0.f_%0.f' % (ptMin*10, ptMax*10))
+        hGenPtFD.SetName(f'hFDGenPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
         fd_gen_list.append(hGenPtFD)
         hGenPtFD.Write()
         if enableSecPeak:
             sparseGen['GenSecPeakPrompt'].GetAxis(0).SetRange(binGenMin, binGenMax)
             sparseGen['GenSecPeakFD'].GetAxis(0).SetRange(binGenMin, binGenMax)
             hGenPtPromptSecPeak = sparseGen['GenSecPeakPrompt'].Projection(0)
-            hGenPtPromptSecPeak.SetName('hPromptSecPeakGenPt_%0.f_%0.f' % (ptMin*10, ptMax*10))
+            hGenPtPromptSecPeak.SetName(f'hPromptSecPeakGenPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
             prompt_gen_list_secpeak.append(hGenPtPromptSecPeak)
             hGenPtPromptSecPeak.Write()
             hGenPtFDSecPeak = sparseGen['GenSecPeakFD'].Projection(0)
-            hGenPtFDSecPeak.SetName('hFDSecPeakGenPt_%0.f_%0.f' % (ptMin*10, ptMax*10))
+            hGenPtFDSecPeak.SetName(f'hFDSecPeakGenPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
             fd_gen_list_secpeak.append(hGenPtFDSecPeak)
             hGenPtFDSecPeak.Write()
 
@@ -139,57 +142,49 @@ for iPt, (ptMin, ptMax) in enumerate(zip(cutVars['Pt']['min'], cutVars['Pt']['ma
                     cutVars[iVar]['axisnum']).SetRange(-1, -1)
 
 for iPt in range(0, len(cutVars['Pt']['min']) - 1):
+    ptLowLabel = cutVars['Pt']['min'][iPt] * 10
+    ptHighLabel = cutVars['Pt']['max'][iPt+1] * 10
     for iVar in ('InvMass', 'Pt'):
+        varName = cutVars[iVar]['name']
         hVar_merged = MergeHists([all_dict[iVar][iPt], all_dict[iVar][iPt+1]])
-        hVar_merged.SetName('h%s_%0.f_%0.f' % (cutVars[iVar]['name'], cutVars['Pt']['min'][iPt]*10,
-                                               cutVars['Pt']['max'][iPt+1]*10))
+        hVar_merged.SetName(f'h{varName}_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
         hVar_merged.Write()
         if isMC:
             hVarPrompt_merged = MergeHists([prompt_dict[iVar][iPt], prompt_dict[iVar][iPt+1]])
-            hVarPrompt_merged.SetName('hPrompt%s_%0.f_%0.f' % (cutVars[iVar]['name'], cutVars['Pt']['min'][iPt]*10,
-                                                               cutVars['Pt']['max'][iPt+1]*10))
+            hVarPrompt_merged.SetName(f'hPrompt{varName}_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
             hVarPrompt_merged.Write()
             hVarFD_merged = MergeHists([fd_dict[iVar][iPt], fd_dict[iVar][iPt+1]])
-            hVarFD_merged.SetName('hFD%s_%0.f_%0.f' % (cutVars[iVar]['name'], cutVars['Pt']['min'][iPt]*10,
-                                                       cutVars['Pt']['max'][iPt+1]*10))
+            hVarFD_merged.SetName(f'hFD{varName}_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
             hVarFD_merged.Write()
             if enableSecPeak:
                 hVarPrompt_secpeak_merged = MergeHists([prompt_dict_secpeak[iVar][iPt],
                                                         prompt_dict_secpeak[iVar][iPt+1]])
-                hVarPrompt_secpeak_merged.SetName('hPromptSecPeak%s_%0.f_%0.f' % (cutVars[iVar]['name'],
-                                                                                  cutVars['Pt']['min'][iPt]*10,
-                                                                                  cutVars['Pt']['max'][iPt+1]*10))
+                hVarPrompt_secpeak_merged.SetName(f'hPromptSecPeak{varName}_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
                 hVarPrompt_secpeak_merged.Write()
                 hVarFD_secpeak_merged = MergeHists([fd_dict_secpeak[iVar][iPt], fd_dict_secpeak[iVar][iPt+1]])
-                hVarFD_secpeak_merged.SetName('hFDSecPeak%s_%0.f_%0.f' % (cutVars[iVar]['name'],
-                                                                          cutVars['Pt']['min'][iPt]*10,
-                                                                          cutVars['Pt']['max'][iPt+1]*10))
+                hVarFD_secpeak_merged.SetName(f'hFDSecPeak{varName}_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
                 hVarFD_secpeak_merged.Write()
 
     if isMC:
         hVarPromptGen_merged = MergeHists([prompt_gen_list[iPt], prompt_gen_list[iPt+1]])
-        hVarPromptGen_merged.SetName('hPromptGenPt_%0.f_%0.f' % (cutVars['Pt']['min'][iPt]*10,
-                                                                 cutVars['Pt']['max'][iPt+1]*10))
+        hVarPromptGen_merged.SetName(f'hPromptGenPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
         hVarPromptGen_merged.Write()
         hVarPromptFD_merged = MergeHists([fd_gen_list[iPt], fd_gen_list[iPt+1]])
-        hVarPromptFD_merged.SetName('hFDGenPt_%0.f_%0.f' % (cutVars['Pt']['min'][iPt]*10,
-                                                            cutVars['Pt']['max'][iPt+1]*10))
+        hVarPromptFD_merged.SetName(f'hFDGenPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
         hVarPromptFD_merged.Write()
         if enableSecPeak:
             hVarPromptGen_secpeak_merged = MergeHists([prompt_gen_list_secpeak[iPt], prompt_gen_list_secpeak[iPt+1]])
-            hVarPromptGen_secpeak_merged.SetName('hPromptGenSecPeakPt_%0.f_%0.f' %
-                                                 (cutVars['Pt']['min'][iPt]*10, cutVars['Pt']['max'][iPt+1]*10))
+            hVarPromptGen_secpeak_merged.SetName(f'hPromptGenSecPeakPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
             hVarPromptGen_secpeak_merged.Write()
             hVarFDGen_secpeak_merged = MergeHists([fd_gen_list_secpeak[iPt], fd_gen_list_secpeak[iPt+1]])
-            hVarFDGen_secpeak_merged.SetName('hFDGenSecPeakPt_%0.f_%0.f' % (cutVars['Pt']['min'][iPt]*10,
-                                                                            cutVars['Pt']['max'][iPt+1]*10))
+            hVarFDGen_secpeak_merged.SetName(f'hFDGenSecPeakPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
             hVarFDGen_secpeak_merged.Write()
 
 hEvForNorm = TH1F("hEvForNorm", ";;Number of events", 2, 0., 2.)
 hEvForNorm.GetXaxis().SetBinLabel(1, "norm counter")
 hEvForNorm.GetXaxis().SetBinLabel(2, "accepted events")
 hEvForNorm.SetBinContent(1, normCounter.GetNEventsForNorm())
-for iBin in range(1, hEv.GetNbinsX()+1):
+for iBin in range(1, hEv.GetNbinsX() + 1):
     binLabel = hEv.GetXaxis().GetBinLabel(iBin)
     if 'isEvSelected' in binLabel or 'accepted' in binLabel:
         hEvForNorm.SetBinContent(2, hEv.GetBinContent(iBin))
--- ProjectDplusDsTree.py
+++ ProjectDplusDsTree.py
@@ -3,18 +3,14 @@ python script for the projection of of D+ and Ds+ mesons TTrees
 run: python ProjectDplusDsTree.py cfgFileName.yml cutSetFileName.yml outFileName.root
 '''
 
+import sys
 import argparse
 import yaml
 from root_numpy import fill_hist
 from ROOT import TFile, TH1F, TDatabasePDG # pylint: disable=import-error,no-name-in-module
 from utils.TaskFileLoader import LoadNormObjFromTask, LoadSparseFromTask
 from utils.DfUtils import FilterBitDf, LoadDfFromRootOrParquet
-
-
-bitSignal = 0
-bitPrompt = 2
-bitFD = 3
-bitRefl = 4
+from utils.AnalysisUtils import MergeHists
 
 parser = argparse.ArgumentParser(description='Arguments to pass')
 parser.add_argument('cfgFileName', metavar='text', default='cfgFileName.yml',
@@ -33,6 +29,11 @@ if not isinstance(inFileNames, list):
     inFileNames = [inFileNames]
 isMC = inputCfg['isMC']
 
+#define filter bits
+bitSignal = 0
+bitPrompt = 2
+bitFD = 3
+bitRefl = 4
 #define mass binning
 meson = inputCfg['tree']['meson']
 if meson == 'Ds':
@@ -41,16 +42,23 @@ elif meson == 'Dplus':
     mD = TDatabasePDG.Instance().GetParticle(411).Mass()
 else:
     print('Error: only Dplus and Ds mesons supported. Exit!')
-    exit()
+    sys.exit()
+massBins = 400
+massLimLow = mD - 0.2
+massLimHigh = mD + 0.2
+#define pT binning
+ptBins = 500
+ptLimLow = 0.
+ptLimHigh = 50.
 
 #selections to be applied
 with open(args.cutSetFileName, 'r') as ymlCutSetFile:
     cutSetCfg = yaml.load(ymlCutSetFile, yaml.FullLoader)
 cutVars = cutSetCfg['cutvars']
 selToApply = []
-for iPt, _ in enumerate(cutVars['Pt']['min']):
+for iPt in range(len(cutVars['Pt']['min'])):
     selToApply.append('')
-    for iVar, varName in enumerate(cutVars):
+    for varName in cutVars:
         if varName == 'InvMass':
             continue
         if selToApply[iPt] != '':
@@ -85,38 +93,38 @@ for iFile, inFileName in enumerate(inFileNames):
 if isMC:
     dataFramePrompt = LoadDfFromRootOrParquet(inputCfg['tree']['filenamePrompt'], inputCfg['tree']['dirname'],
                                               inputCfg['tree']['treename'])
-
     if 'cand_type' in dataFramePrompt.columns: #if not filtered tree, select only FD and not reflected
         dataFramePrompt = FilterBitDf(dataFramePrompt, 'cand_type', [bitSignal, bitPrompt], 'and')
         dataFramePrompt = FilterBitDf(dataFramePrompt, 'cand_type', [bitRefl], 'not')
 
     dataFrameFD = LoadDfFromRootOrParquet(inputCfg['tree']['filenameFD'], inputCfg['tree']['dirname'],
                                           inputCfg['tree']['treename'])
-
     if 'cand_type' in dataFrameFD.columns: #if not filtered tree, select only FD and not reflected
         dataFrameFD = FilterBitDf(dataFrameFD, 'cand_type', [bitSignal, bitFD], 'and')
         dataFrameFD = FilterBitDf(dataFrameFD, 'cand_type', [bitRefl], 'not')
 
-    for iPt, (cuts, ptMin, ptMax) in enumerate(zip(selToApply, cutVars['Pt']['min'], cutVars['Pt']['max'])):
-        print("Projecting distributions for %0.1f < pT < %0.1f GeV/c" % (ptMin, ptMax))
+    for (cuts, ptMin, ptMax) in zip(selToApply, cutVars['Pt']['min'], cutVars['Pt']['max']):
+        print(f'Projecting distributions for {ptMin:.1f} < pT < {ptMax:.1f} GeV/c')
+        ptLowLabel = ptMin * 10
+        ptHighLabel = ptMax * 10
         #gen histos from sparses
-        binGenMin = sparseGen['GenPrompt'].GetAxis(0).FindBin(ptMin*1.0001)
-        binGenMax = sparseGen['GenPrompt'].GetAxis(0).FindBin(ptMax*0.9999)
+        binGenMin = sparseGen['GenPrompt'].GetAxis(0).FindBin(ptMin * 1.0001)
+        binGenMax = sparseGen['GenPrompt'].GetAxis(0).FindBin(ptMax * 0.9999)
         sparseGen['GenPrompt'].GetAxis(0).SetRange(binGenMin, binGenMax)
         sparseGen['GenFD'].GetAxis(0).SetRange(binGenMin, binGenMax)
         hGenPtPrompt = sparseGen['GenPrompt'].Projection(0)
-        hGenPtPrompt.SetName('hPromptGenPt_{0:.0f}_{1:.0f}'.format(ptMin*10, ptMax*10))
+        hGenPtPrompt.SetName(f'hPromptGenPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
         promptGenList.append(hGenPtPrompt)
         hGenPtFD = sparseGen['GenFD'].Projection(0)
-        hGenPtFD.SetName('hFDGenPt_{0:.0f}_{1:.0f}'.format(ptMin*10, ptMax*10))
+        hGenPtFD.SetName(f'hFDGenPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
         FDGenList.append(hGenPtFD)
         #reco histos from trees
         dataFramePromptSel = dataFramePrompt.astype(float).query(cuts)
         dataFrameFDSel = dataFrameFD.astype(float).query(cuts)
-        hPtPrompt = TH1F('hPromptPt_{0:.0f}_{1:.0f}'.format(ptMin*10, ptMax*10), '', 500, 0., 50.)
-        hInvMassPrompt = TH1F('hPromptMass_{0:.0f}_{1:.0f}'.format(ptMin*10, ptMax*10), '', 400, mD-0.2, mD+0.2)
-        hPtFD = TH1F('hFDPt_{0:.0f}_{1:.0f}'.format(ptMin*10, ptMax*10), '', 500, 0., 50.)
-        hInvMassFD = TH1F('hFDMass_{0:.0f}_{1:.0f}'.format(ptMin*10, ptMax*10), '', 400, mD-0.2, mD+0.2)
+        hPtPrompt = TH1F(f'hPromptPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}', '', ptBins, ptLimLow, ptLimHigh)
+        hInvMassPrompt = TH1F(f'hPromptMass_{ptLowLabel:.0f}_{ptHighLabel:.0f}', '', massBins, massLimLow, massLimHigh)
+        hPtFD = TH1F(f'hFDPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}', '', ptBins, ptLimLow, ptLimHigh)
+        hInvMassFD = TH1F(f'hFDMass_{ptLowLabel:.0f}_{ptHighLabel:.0f}', '', massBins, massLimLow, massLimHigh)
         fill_hist(hPtPrompt, dataFramePromptSel['pt_cand'])
         fill_hist(hInvMassPrompt, dataFramePromptSel['inv_mass'])
         fill_hist(hPtFD, dataFrameFDSel['pt_cand'])
@@ -125,7 +133,6 @@ if isMC:
         promptDict['Pt'].append(hPtPrompt)
         FDDict['InvMass'].append(hInvMassFD)
         FDDict['Pt'].append(hPtFD)
-
         outFile.cd()
         hGenPtPrompt.Write()
         hGenPtFD.Write()
@@ -133,36 +140,70 @@ if isMC:
         hInvMassPrompt.Write()
         hPtFD.Write()
         hInvMassFD.Write()
-else:
 
+    #merge adiacent pt bin histograms
+    for iPt in range(0, len(cutVars['Pt']['min']) - 1):
+        ptLowLabel = cutVars['Pt']['min'][iPt] * 10
+        ptHighLabel = cutVars['Pt']['max'][iPt+1] * 10
+        hPtPromptMerged = MergeHists([promptDict['Pt'][iPt], promptDict['Pt'][iPt+1]])
+        hPtPromptMerged.SetName(f'hPromptPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
+        hPtPromptMerged.Write()
+        hInvMassPromptMerged = MergeHists([promptDict['InvMass'][iPt], promptDict['InvMass'][iPt+1]])
+        hInvMassPromptMerged.SetName(f'hPromptMass_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
+        hInvMassPromptMerged.Write()
+        hPtFDMerged = MergeHists([FDDict['Pt'][iPt], FDDict['Pt'][iPt+1]])
+        hPtFDMerged.SetName(f'hFDPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
+        hPtFDMerged.Write()
+        hInvMassFDMerged = MergeHists([FDDict['InvMass'][iPt], FDDict['InvMass'][iPt+1]])
+        hInvMassFDMerged.SetName(f'hFDMass_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
+        hInvMassFDMerged.Write()
+        hPtPromptGenMerged = MergeHists([promptGenList[iPt], promptGenList[iPt+1]])
+        hPtPromptGenMerged.SetName(f'hPromptGenPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
+        hPtPromptGenMerged.Write()
+        hPtPromptFDMerged = MergeHists([FDGenList[iPt], FDGenList[iPt+1]])
+        hPtPromptFDMerged.SetName(f'hFDGenPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
+        hPtPromptFDMerged.Write()
+
+else:
     dataFrame = LoadDfFromRootOrParquet(inputCfg['tree']['filenameAll'], inputCfg['tree']['dirname'],
                                         inputCfg['tree']['treename'])
 
-    for iPt, (cuts, ptMin, ptMax) in enumerate(zip(selToApply, cutVars['Pt']['min'], cutVars['Pt']['max'])):
-        print("Projecting distributions for %0.1f < pT < %0.1f GeV/c" % (ptMin, ptMax))
+    for (cuts, ptMin, ptMax) in zip(selToApply, cutVars['Pt']['min'], cutVars['Pt']['max']):
+        print(f'Projecting distributions for {ptMin:.1f} < pT < {ptMax:.1f} GeV/c')
+        ptLowLabel = ptMin * 10
+        ptHighLabel = ptMax * 10
         dataFrameSel = dataFrame.astype(float).query(cuts)
-        hPt = TH1F('hPt_{0:.0f}_{1:.0f}'.format(ptMin*10, ptMax*10), '', 500, 0., 50.)
-        hInvMass = TH1F('hMass_{0:.0f}_{1:.0f}'.format(ptMin*10, ptMax*10), '', 400, mD-0.2, mD+0.2)
+        hPt = TH1F(f'hPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}', '', ptBins, ptLimLow, ptLimHigh)
+        hInvMass = TH1F(f'hMass_{ptLowLabel:.0f}_{ptHighLabel:.0f}', '', massBins, massLimLow, massLimHigh)
         fill_hist(hPt, dataFrameSel['pt_cand'])
         fill_hist(hInvMass, dataFrameSel['inv_mass'])
         allDict['InvMass'].append(hInvMass)
         allDict['Pt'].append(hPt)
-
         outFile.cd()
         hPt.Write()
         hInvMass.Write()
+
+    #merge adiacent pt bin histograms
+    for iPt in range(0, len(cutVars['Pt']['min']) - 1):
+        ptLowLabel = cutVars['Pt']['min'][iPt] * 10
+        ptHighLabel = cutVars['Pt']['max'][iPt+1] * 10
+        hPtMerged = MergeHists([allDict['Pt'][iPt], allDict['Pt'][iPt+1]])
+        hPtMerged.SetName(f'hPt_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
+        hPtMerged.Write()
+        hInvMassMerged = MergeHists([allDict['InvMass'][iPt], allDict['InvMass'][iPt+1]])
+        hInvMassMerged.SetName(f'hMass_{ptLowLabel:.0f}_{ptHighLabel:.0f}')
+        hInvMassMerged.Write()
 
 #normalisation
 hEvForNorm = TH1F("hEvForNorm", ";;Number of events", 2, 0., 2.)
 hEvForNorm.GetXaxis().SetBinLabel(1, "norm counter")
 hEvForNorm.GetXaxis().SetBinLabel(2, "accepted events")
 hEvForNorm.SetBinContent(1, normCounter.GetNEventsForNorm())
-for iBin in range(1, hEv.GetNbinsX()+1):
+for iBin in range(1, hEv.GetNbinsX() + 1):
     binLabel = hEv.GetXaxis().GetBinLabel(iBin)
     if 'isEvSelected' in binLabel or 'accepted' in binLabel:
         hEvForNorm.SetBinContent(2, hEv.GetBinContent(iBin))
         break
-
 outFile.cd()
 hEvForNorm.Write()
 outFile.Close()
