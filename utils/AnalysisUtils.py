650c35689a14e704cf819149043dd8c0e5de11eb
Fabrizio <fabrizio.grosa@cern.ch>
2020-04-22 16:00:56 +0200

    Modify the decreasing/increasing variations wrt the central point (#96)
    * Modify the decreasing/increasing variations with respect to the central point

--- utils/AnalysisUtils.py
+++ utils/AnalysisUtils.py
@@ -735,8 +735,8 @@ def ApplyVariationToList(listToVary, relVar, option='decreasing'):
     elif option == 'downshift':
         listVaried = [el - el*relVar for el in listToVary]
     elif option == 'decreasing':
-        listVaried = [el - el*relVar/len(listToVary)*(iEl+1) for iEl, el in enumerate(listToVary)]
+        listVaried = [el - el*relVar/len(listToVary)*(iEl+1) + relVar*el/2 for iEl, el in enumerate(listToVary)]
     elif option == 'increasing':
-        listVaried = [el + el*relVar/len(listToVary)*(iEl+1) for iEl, el in enumerate(listToVary)]
+        listVaried = [el + el*relVar/len(listToVary)*(iEl+1) - relVar*el/2 for iEl, el in enumerate(listToVary)]
 
     return listVaried
