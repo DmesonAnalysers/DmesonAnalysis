2960c5f1ec390092374892bfcc72858e25c940d1
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2020-07-13 11:32:04 +0200

    Improve script for efficiency comparison

--- comparisons/CompareEfficiencies.py
+++ comparisons/CompareEfficiencies.py
@@ -2,19 +2,20 @@ import sys
 from os.path import join
 from ROOT import TCanvas, TFile, TLegend # pylint: disable=import-error,no-name-in-module
 from ROOT import kRed, kBlack, kBlue, kAzure, kFullCircle, kFullSquare, kFullDiamond # pylint: disable=import-error,no-name-in-module,unused-import
+#pylint: disable=wrong-import-position,import-error,no-name-in-module
 sys.path.append('..')
-from utils.StyleFormatter import SetGlobalStyle, SetObjectStyle #pylint: disable=wrong-import-position,import-error,no-name-in-module
+from utils.AnalysisUtils import ComputeRatioDiffBins
+from utils.StyleFormatter import SetGlobalStyle, SetObjectStyle
 
-inDir = '~/Desktop/Analyses/pp5TeV/Ds_wML_mult/outputs/100320'
-inFileNames = ['eff/Efficiency_Ds_FDen_pt2_12_conservative.root',
-               'systematics/eff_noImprover/Efficiency_Ds_FDen_conservative_pt2_12_noImprover.root']
-histoNames = ['hEff', 'hEff']
-colors = [kRed, kBlue]
+inDir = 'inputdir'
+inFileNames = ['file1.root', 'file2.root']
+histoNames = ['hAccEff', 'hAccEff']
+colors = [kRed+1, kAzure+4]
 markers = [kFullSquare, kFullCircle]
-legNames = ['Improver', 'No Improver (same ML models)']
-outDir = '~/Desktop/Analyses/pp5TeV/Ds_wML_mult/outputs/100320/systematics/eff_noImprover'
-outSuffix = 'Improver_sameModels'
-showUnc = False
+legNames = ['title1', 'title2']
+outDir = 'outputdir'
+outSuffix = 'suffix'
+showUnc = True
 
 SetGlobalStyle(padleftmargin=0.16, padtopmargin=0.05, padbottommargin=0.14,
                titleoffsety=1.5, titlesize=0.05, labelsize=0.045)
@@ -33,36 +34,43 @@ for iFile, inFileName in enumerate(inFileNames):
     hEffFD.append(inFile.Get(f'{histoNames[iFile]}FD'))
     hEffPrompt[iFile].SetDirectory(0)
     hEffFD[iFile].SetDirectory(0)
+    hEffPrompt[iFile].GetYaxis().SetRangeUser(1e-4, 1.)
+    hEffFD[iFile].GetYaxis().SetRangeUser(1e-4, 1.)
+    hEffPromptRatio.append(ComputeRatioDiffBins(hEffPrompt[iFile], hEffPrompt[0]))
+    hEffPromptRatio[iFile].SetDirectory(0)
+    hEffPromptRatio[iFile].SetName(f'hEffPromptRatio{iFile}')
+    hEffFDRatio.append(ComputeRatioDiffBins(hEffFD[iFile], hEffFD[0]))
+    hEffFDRatio[iFile].SetDirectory(0)
+    hEffFDRatio[iFile].SetName(f'hEffFDRatio{iFile}')
     SetObjectStyle(hEffPrompt[iFile], linecolor=colors[iFile], markercolor=colors[iFile], markerstyle=markers[iFile])
     SetObjectStyle(hEffFD[iFile], linecolor=colors[iFile], markercolor=colors[iFile],
                    markerstyle=markers[iFile], linestyle=1)
-    hEffPrompt[iFile].GetYaxis().SetRangeUser(1e-3, 1.)
-    hEffFD[iFile].GetYaxis().SetRangeUser(1e-3, 1.)
-    hEffPromptRatio.append(hEffPrompt[iFile].Clone(f'hEffPromptRatio{iFile}'))
-    hEffPromptRatio[iFile].SetDirectory(0)
-    hEffPromptRatio[iFile].Divide(hEffPrompt[iFile], hEffPrompt[0])
-    hEffPromptRatio[iFile].GetYaxis().SetRangeUser(0.8, 1.2)
-    hEffFDRatio.append(hEffFD[iFile].Clone(f'hEffFDRatio{iFile}'))
-    hEffFDRatio[iFile].SetDirectory(0)
-    hEffFDRatio[iFile].Divide(hEffFD[iFile], hEffFD[0])
-    hEffFDRatio[iFile].GetYaxis().SetRangeUser(0.8, 1.2)
+    SetObjectStyle(hEffPromptRatio[iFile], linecolor=colors[iFile], markercolor=colors[iFile],
+                   markerstyle=markers[iFile], linestyle=1)
+    SetObjectStyle(hEffFDRatio[iFile], linecolor=colors[iFile], markercolor=colors[iFile], markerstyle=markers[iFile])
     leg.AddEntry(hEffFD[iFile], legNames[iFile], 'p')
     if not showUnc:
         for iBin in range(hEffPromptRatio[iFile].GetNbinsX()):
             hEffPromptRatio[iFile].SetBinError(iBin+1, 1.e-20)
             hEffFDRatio[iFile].SetBinError(iBin+1, 1.e-20)
 
-ptmin = hEffPrompt[0].GetBinLowEdge(1)
-ptmax = hEffPrompt[0].GetBinLowEdge(hEffPrompt[0].GetNbinsX())+hEffPrompt[0].GetBinWidth(hEffPrompt[0].GetNbinsX())
+PtMin = hEffPrompt[0].GetBinLowEdge(1)
+PtMax = hEffPrompt[0].GetBinLowEdge(hEffPrompt[0].GetNbinsX())+hEffPrompt[0].GetBinWidth(hEffPrompt[0].GetNbinsX())
+for histo in hEffPrompt:
+    if histo.GetBinLowEdge(1) < PtMin:
+        PtMin = histo.GetBinLowEdge(1)
+    if histo.GetBinLowEdge(histo.GetNbinsX())+histo.GetBinWidth(histo.GetNbinsX()) > PtMax:
+        PtMax = histo.GetBinLowEdge(histo.GetNbinsX())+histo.GetBinWidth(histo.GetNbinsX())
 
 cPrompt = TCanvas('cPrompt', '', 1000, 500)
 cPrompt.Divide(2, 1)
-cPrompt.cd(1).DrawFrame(ptmin, 1.e-4, ptmax, 1., ';#it{p}_{T} (GeV/#it{c}); Prompt efficiency')
+cPrompt.cd(1).DrawFrame(PtMin, hEffPrompt[0].GetMinimum()/5, PtMax, 1., ';#it{p}_{T} (GeV/#it{c}); Prompt efficiency')
 cPrompt.cd(1).SetLogy()
 for iFile in range(len(inFileNames)):
     hEffPrompt[iFile].Draw('same')
 leg.Draw()
-cPrompt.cd(2).DrawFrame(ptmin, 0.5, ptmax, 1.5, ';#it{p}_{T} (GeV/#it{c}); Prompt efficiency ratio')
+cPrompt.cd(2).DrawFrame(PtMin, hEffPromptRatio[1].GetMinimum()/2, PtMax, hEffPromptRatio[1].GetMaximum()*2,
+                        ';#it{p}_{T} (GeV/#it{c}); Prompt efficiency ratio')
 for iFile in range(len(inFileNames)):
     if iFile == 0:
         continue
@@ -70,12 +78,13 @@ for iFile in range(len(inFileNames)):
 
 cFD = TCanvas('cFD', '', 1000, 500)
 cFD.Divide(2, 1)
-cFD.cd(1).DrawFrame(ptmin, 1.e-4, ptmax, 1., ';#it{p}_{T} (GeV/#it{c}); Feed-down efficiency')
+cFD.cd(1).DrawFrame(PtMin, hEffFD[0].GetMinimum()/5, PtMax, 1., ';#it{p}_{T} (GeV/#it{c}); Feed-down efficiency')
 cFD.cd(1).SetLogy()
 for iFile in range(len(inFileNames)):
     hEffFD[iFile].Draw('same')
 leg.Draw()
-cFD.cd(2).DrawFrame(ptmin, 0.5, ptmax, 1.5, ';#it{p}_{T} (GeV/#it{c}); Feed-down efficiency ratio')
+cFD.cd(2).DrawFrame(PtMin, hEffFDRatio[1].GetMinimum()/2, PtMax, hEffFDRatio[1].GetMaximum()*2,
+                    ';#it{p}_{T} (GeV/#it{c}); Feed-down efficiency ratio')
 for iFile in range(len(inFileNames)):
     if iFile == 0:
         continue
