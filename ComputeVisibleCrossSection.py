150acb8a54ef73be77ee9f2034338c77c2363629
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2021-01-27 17:54:24 +0100

    Add possibility to compute visible cross section in Pb-Pb and for D*

--- ComputeVisibleCrossSection.py
+++ ComputeVisibleCrossSection.py
@@ -19,18 +19,24 @@ parser.add_argument('outFileName', metavar='text', default='outFile.root', help=
 parser.add_argument('--Dplus', action='store_true', default=False, help='enable calculation for D+')
 parser.add_argument('--Ds', action='store_true', default=False, help='enable calculation for Ds')
 parser.add_argument('--Dzero', action='store_true', default=False, help='enable calculation for D0')
+parser.add_argument('--Dstar', action='store_true', default=False, help='enable calculation for D*')
+parser.add_argument('--PbPb', action='store_true', default=True, help='enable calculation for PbPb collisions')
 parser.add_argument('--ptmin', default=0., type=float, help='minimum pT')
 parser.add_argument('--ptmax', default=1.e10, type=float, help='maximum pT')
 args = parser.parse_args()
 
-if not args.Dplus and not args.Ds and not args.Dzero:
-    print('ERROR: you should enable the comparison for either D+, Ds, or Dzero! Exit')
+argChannels = [args.Dplus, args.Ds, args.Dzero, args.Dstar]
+if not any(argChannels):
+    print('ERROR: you should enable the comparison for either Dplus, Ds, Dzero, or Dstar! Exit')
     sys.exit()
-elif (args.Dplus and args.Ds) or (args.Dplus and args.Dzero) or (args.Ds and args.Dzero):
+elif len(np.where(argChannels)[0]) > 1:
     print('ERROR: you can enable only one meson at a time! Exit')
     sys.exit()
 
-lumiUnc = 0.021 # 2.1%
+if not args.PbPb:
+    lumiUnc = 0.021 # pp@5.02 TeV
+else:
+    lumiUnc = 0.
 
 if args.Dplus:
     BR = 0.0938
@@ -44,6 +50,10 @@ elif args.Dzero:
     BR = 0.03950
     BRunc = 0.00031
     mesonName = 'D^{0}'
+elif args.Dstar:
+    BR = 0.03950*0.677
+    BRunc = np.sqrt(0.00031**2/0.03950**2 + 0.005*2/0.677**2) * BR
+    mesonName = 'D*^{+}'
 
 SetGlobalStyle()
 
@@ -53,25 +63,39 @@ inFileCross = TFile.Open(args.inFileNamePtDiffCrossSec)
 hCrossSection = inFileCross.Get('hCrossSection')
 isDataDriven = True
 if not hCrossSection:
-    hCrossSection = inFileCross.Get('histoSigmaCorr')
-    hCrossSection.Scale(1.e-6 / BR)
+    if not args.PbPb:
+        hCrossSection = inFileCross.Get('histoSigmaCorr')
+        gCrossSectionFDSyst = inFileCross.Get('gSigmaCorrConservative')
+        gCrossSectionFDSyst.RemovePoint(0)
+        hCrossSection.Scale(1.e-6 / BR)
+        ScaleGraph(gCrossSectionFDSyst, 1.e-6 / BR)
+    else:
+        hCrossSection = inFileCross.Get('hAAC')
+        gCrossSectionFDSyst = inFileCross.Get('gaaCsystFD')
     hCrossSection.SetStats(0)
-    gCrossSectionFDSyst = inFileCross.Get('gSigmaCorrConservative')
-    gCrossSectionFDSyst.RemovePoint(0)
-    ScaleGraph(gCrossSectionFDSyst, 1.e-6 / BR)
     isDataDriven = False
 hCrossSection.SetDirectory(0)
-if not args.Dzero:
-    HFsystErr.append(inFileCross.Get('AliHFSystErr'))
+if not args.PbPb:
+    if not args.Dzero:
+        HFsystErr.append(inFileCross.Get('AliHFSystErr'))
+    else:
+        HFsystErr.append(inFileCross.Get('AliHFSystErrTopol'))
+        HFsystErr.append(inFileCross.Get('AliHFSystErrLowpt'))
 else:
-    HFsystErr.append(inFileCross.Get('AliHFSystErrTopol'))
-    HFsystErr.append(inFileCross.Get('AliHFSystErrLowpt'))
+    if not args.Dzero:
+        HFsystErr.append(inFileCross.Get('AliHFSystErrAA'))
+    else:
+        if inFileCross.Get('AliHFSystErrAA1'):
+            HFsystErr.append(inFileCross.Get('AliHFSystErrAA1'))
+            HFsystErr.append(inFileCross.Get('AliHFSystErrAA2'))
+        else:
+            HFsystErr.append(inFileCross.Get('AliHFSystErrAA'))
 
 inFileCross.Close()
 
 # compute visible cross section
 visCrossSec, statUnc, uncorrSystUnc, corrSystUncLow, corrSystUncHigh, \
-    FDSystUncLow, FDSystUncHigh, trackSystUnc = (0 for _ in range(8))
+    FDSystUncLow, FDSystUncHigh, trackSystUnc, systWoTrFDBRLumi = (0 for _ in range(9))
 
 for iPt in range(hCrossSection.GetNbinsX()):
 
@@ -94,15 +118,17 @@ for iPt in range(hCrossSection.GetNbinsX()):
         systErr = HFsystErr[0] # topological
 
     # uncorrelated systematic uncertainty (yield extraction)
-    uncorrSystUnc += systErr.GetRawYieldErr(ptCent)**2
+    uncorrSystUnc += systErr.GetRawYieldErr(ptCent)**2 * hCrossSection.GetBinContent(iPt+1)**2 * binWidth**2
 
     # correlated systematic uncertainty (sel eff, PID eff, gen pT shape, tracking, FD, BR)
-    trackSystUnc += systErr.GetTrackingEffErr(ptCent) * binWidth
+    trackSystUnc += systErr.GetTrackingEffErr(ptCent) * hCrossSection.GetBinContent(iPt+1) * binWidth
+    systWoTrFDBRLumi += (systErr.GetCutsEffErr(ptCent)**2 + systErr.GetMCPtShapeErr(ptCent)**2 + \
+        systErr.GetPIDEffErr(ptCent)**2) * hCrossSection.GetBinContent(iPt+1)**2 * binWidth**2
     totCorrSystUncSqLow = (systErr.GetCutsEffErr(ptCent)**2 + systErr.GetMCPtShapeErr(ptCent)**2 + \
-        systErr.GetPIDEffErr(ptCent)**2 + systErr.GetTrackingEffErr(ptCent)**2 + BRunc**2/BR**2) \
+        systErr.GetPIDEffErr(ptCent)**2 + systErr.GetTrackingEffErr(ptCent)**2) \
             * hCrossSection.GetBinContent(iPt+1)**2 * binWidth**2
     totCorrSystUncSqHigh = (systErr.GetCutsEffErr(ptCent)**2 + systErr.GetMCPtShapeErr(ptCent)**2 + \
-        systErr.GetPIDEffErr(ptCent)**2 + systErr.GetTrackingEffErr(ptCent)**2 + BRunc**2/BR**2) \
+        systErr.GetPIDEffErr(ptCent)**2 + systErr.GetTrackingEffErr(ptCent)**2) \
             * hCrossSection.GetBinContent(iPt+1)**2 * binWidth**2
     if isDataDriven:
         FDSystUncLow += systErr.GetDataDrivenFDErr(ptCent) * binWidth
@@ -120,13 +146,15 @@ for iPt in range(hCrossSection.GetNbinsX()):
         FDSystUncLow += gCrossSectionFDSyst.GetErrorYlow(iPtFD) * binWidth
         FDSystUncHigh += gCrossSectionFDSyst.GetErrorYlow(iPtFD) * binWidth
         totCorrSystUncSqLow += gCrossSectionFDSyst.GetErrorYlow(iPtFD)**2 * binWidth**2
-        totCorrSystUncSqLow += gCrossSectionFDSyst.GetErrorYhigh(iPtFD)**2 * binWidth**2
+        totCorrSystUncSqHigh += gCrossSectionFDSyst.GetErrorYhigh(iPtFD)**2 * binWidth**2
 
     corrSystUncLow += np.sqrt(totCorrSystUncSqLow)
     corrSystUncHigh += np.sqrt(totCorrSystUncSqHigh)
 
 statUnc = np.sqrt(statUnc)
 uncorrSystUnc = np.sqrt(uncorrSystUnc)
+corrSystUncLow = np.sqrt(corrSystUncLow**2 + (lumiUnc**2 + BRunc**2/BR**2)*visCrossSec)
+corrSystUncHigh = np.sqrt(corrSystUncHigh**2 + (lumiUnc**2 + BRunc**2/BR**2)*visCrossSec)
 totSystUncLow = np.sqrt(uncorrSystUnc**2 + corrSystUncLow**2)
 totSystUncHigh = np.sqrt(uncorrSystUnc**2 + corrSystUncHigh**2)
 
@@ -140,45 +168,54 @@ if args.ptmax >= hCrossSection.GetXaxis().GetBinUpEdge(hCrossSection.GetNbinsX()
 else:
     ptMax = args.ptmax
 
+graphName = 'VisCrossSec'
 utilStr = f';;d#sigma/d#it{{y}} (#mub) ({ptMin} < #it{{p}}_{{T}} < {ptMax} GeV/#it{{c}})'
-hVisibleCrossSectionStat = TH1F('hVisibleCrossSectionStat', utilStr, 1, 0.5, 1.5)
+if args.PbPb:
+    graphName = 'PtVisYield'
+    utilStr = f';;d#it{{N}}/d#it{{y}} ({ptMin} < #it{{p}}_{{T}} < {ptMax} GeV/#it{{c}})'
 
-gVisCrossSecUncorrSyst, gVisCrossSecCorrSyst, gVisCrossSecTotSyst, gVisCrossSecSystWoTrFDAndLumi, \
-    gVisCrossSecSystTracking, gVisCrossSecSystFD, gVisCrossSecSystLumi = (TGraphAsymmErrors(0) for _ in range(7))
-gVisCrossSecUncorrSyst.SetNameTitle('gVisCrossSecUncorrSyst', utilStr)
-gVisCrossSecCorrSyst.SetNameTitle('gVisCrossSecCorrSyst', utilStr)
-gVisCrossSecTotSyst.SetNameTitle('gVisCrossSecTotSyst', utilStr)
-gVisCrossSecSystWoTrFDAndLumi.SetNameTitle('gVisCrossSecSystWoTrFDAndLumi', utilStr)
-gVisCrossSecSystTracking.SetNameTitle('gVisCrossSecSystTracking', utilStr)
-gVisCrossSecSystFD.SetNameTitle('gVisCrossSecSystFD', utilStr)
-gVisCrossSecSystLumi.SetNameTitle('gVisCrossSecSystLumi', utilStr)
+hVisibleCrossSectionStat = TH1F(f'h{graphName}', utilStr, 1, 0.5, 1.5)
+
+gVisCrossSecUncorrSyst, gVisCrossSecCorrSyst, gVisCrossSecTotSyst, gVisCrossSecSystWoTrFDBRAndLumi, \
+    gVisCrossSecSystTracking, gVisCrossSecSystFD, gVisCrossSecSystLumi, gVisCrossSecSystBR = \
+        (TGraphAsymmErrors(0) for _ in range(8))
+gVisCrossSecUncorrSyst.SetNameTitle(f'g{graphName}UncorrSys', utilStr)
+gVisCrossSecCorrSyst.SetNameTitle(f'g{graphName}PtCorrSys', utilStr)
+gVisCrossSecTotSyst.SetNameTitle(f'g{graphName}TotSys', utilStr)
+gVisCrossSecSystWoTrFDBRAndLumi.SetNameTitle(f'g{graphName}SysWoTrFDBRAndLumi', utilStr)
+gVisCrossSecSystTracking.SetNameTitle('gVisCrossSecSysTracking', utilStr)
+gVisCrossSecSystFD.SetNameTitle(f'g{graphName}SysFD', utilStr)
+gVisCrossSecSystLumi.SetNameTitle(f'g{graphName}SysLumi', utilStr)
+gVisCrossSecSystBR.SetNameTitle(f'g{graphName}SysBR', utilStr)
 
 SetObjectStyle(hVisibleCrossSectionStat, color=kBlack, markerstyle=kFullCircle)
 SetObjectStyle(gVisCrossSecUncorrSyst, color=kBlack, fillstyle=0)
 SetObjectStyle(gVisCrossSecCorrSyst, color=kBlack, fillstyle=0)
 SetObjectStyle(gVisCrossSecTotSyst, color=kBlack, fillstyle=0)
 SetObjectStyle(gVisCrossSecSystLumi, color=kBlack, fillstyle=0)
+SetObjectStyle(gVisCrossSecSystBR, color=kBlack, fillstyle=0)
 SetObjectStyle(gVisCrossSecSystFD, color=kBlack, fillstyle=0)
 SetObjectStyle(gVisCrossSecSystTracking, color=kBlack, fillstyle=0)
-SetObjectStyle(gVisCrossSecSystWoTrFDAndLumi, color=kBlack, fillstyle=0)
+SetObjectStyle(gVisCrossSecSystWoTrFDBRAndLumi, color=kBlack, fillstyle=0)
 
 hVisibleCrossSectionStat.SetBinContent(1, visCrossSec)
 hVisibleCrossSectionStat.SetBinError(1, statUnc)
 gVisCrossSecUncorrSyst.SetPoint(0, 1., visCrossSec)
 gVisCrossSecCorrSyst.SetPoint(0, 1., visCrossSec)
 gVisCrossSecTotSyst.SetPoint(0, 1., visCrossSec)
-gVisCrossSecSystWoTrFDAndLumi.SetPoint(0, 1., visCrossSec)
+gVisCrossSecSystWoTrFDBRAndLumi.SetPoint(0, 1., visCrossSec)
 gVisCrossSecSystTracking.SetPoint(0, 1., visCrossSec)
 gVisCrossSecSystFD.SetPoint(0, 1., visCrossSec)
 gVisCrossSecSystLumi.SetPoint(0, 1., visCrossSec)
+gVisCrossSecSystBR.SetPoint(0, 1., visCrossSec)
 gVisCrossSecUncorrSyst.SetPointError(0, 0.3, 0.3, uncorrSystUnc, uncorrSystUnc)
 gVisCrossSecCorrSyst.SetPointError(0, 0.3, 0.3, corrSystUncLow, corrSystUncHigh)
 gVisCrossSecTotSyst.SetPointError(0, 0.3, 0.3, totSystUncLow, totSystUncHigh)
-gVisCrossSecSystWoTrFDAndLumi.SetPointError(0, 0.3, 0.3, np.sqrt(totSystUncLow**2 - trackSystUnc**2 - FDSystUncLow**2),
-                                            np.sqrt(totSystUncHigh**2 - trackSystUnc**2 - FDSystUncHigh**2))
+gVisCrossSecSystWoTrFDBRAndLumi.SetPointError(0, 0.3, 0.3, systWoTrFDBRLumi, systWoTrFDBRLumi)
 gVisCrossSecSystTracking.SetPointError(0, 0.3, 0.3, trackSystUnc, trackSystUnc)
 gVisCrossSecSystFD.SetPointError(0, 0.3, 0.3, FDSystUncLow, FDSystUncHigh)
 gVisCrossSecSystLumi.SetPointError(0, 0.3, 0.3, visCrossSec*lumiUnc, visCrossSec*lumiUnc)
+gVisCrossSecSystBR.SetPointError(0, 0.3, 0.3, visCrossSec*BRunc/BR, visCrossSec*BRunc/BR)
 
 # otput file
 outFile = TFile.Open(args.outFileName, 'recreate')
@@ -186,10 +223,12 @@ hVisibleCrossSectionStat.Write()
 gVisCrossSecUncorrSyst.Write()
 gVisCrossSecCorrSyst.Write()
 gVisCrossSecTotSyst.Write()
-gVisCrossSecSystWoTrFDAndLumi.Write()
+gVisCrossSecSystWoTrFDBRAndLumi.Write()
 gVisCrossSecSystTracking.Write()
 gVisCrossSecSystFD.Write()
-gVisCrossSecSystLumi.Write()
+gVisCrossSecSystBR.Write()
+if not args.PbPb:
+    gVisCrossSecSystLumi.Write()
 outFile.Close()
 
 print(f'Saved output file: {args.outFileName}')
