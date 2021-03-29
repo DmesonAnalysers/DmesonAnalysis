ca73ebc974b2107a7d22a792273868a9b1b754e7
danielbattistini <daniel.battistini@edu.unito.it>
2021-03-29 08:00:02 +0200

    Add debug function

--- utils/Misc.py
+++ utils/Misc.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 def exec_time(start, stop):
     '''
@@ -20,3 +21,10 @@ def exec_time(start, stop):
     seconds = math.floor((stop - start - hours*3600 - minutes*60))
 
     print(f'Execution time: {hours}:{minutes}:{seconds}')
+
+def Debug(msg = '', printMessageInfo=True):
+    '''
+    Prints the file name and the line in the code in which this function is called with a message
+    '''
+    if printMessageInfo:
+        print(f"debug file {sys.argv[0]} line {sys._getframe().f_back.f_lineno}: {msg}")
