9eafb196344a3cad7089832dd5b6972646b05ec8
Fabio Catalano <fabio.catalano@cern.ch>
2021-05-31 11:47:50 +0200

    Update utility script for raw-yield systematics

--- systematics/rawyields/PlotMultiTrialRMSvsPt.py
+++ systematics/rawyields/PlotMultiTrialRMSvsPt.py
@@ -5,7 +5,7 @@ run: python PlotMultiTrialRMSvsPt MultiTrial.root RawYieldsDefault.root Output.p
 
 import sys
 import argparse
-from ROOT import TCanvas, TFile, TGaxis, gPad # pylint: disable=import-error,no-name-in-module
+from ROOT import TCanvas, TFile, TGaxis, gPad, TMath # pylint: disable=import-error,no-name-in-module
 from ROOT import kBlack, kRed # pylint: disable=import-error,no-name-in-module
 sys.path.append('../..')
 from utils.StyleFormatter import SetGlobalStyle, SetObjectStyle #pylint: disable=wrong-import-position,import-error
@@ -34,16 +34,24 @@ nPtBins = hRawYields.GetNbinsX()
 hRawYieldDistr = []
 hRMS = hRawYields.Clone('hRMS')
 hRMS.Reset()
+hMeanShift = hRawYields.Clone('hMeanShift')
+hMeanShift.Reset()
+hSyst = hRawYields.Clone('hSyst')
+hSyst.Reset()
 inFileMT = TFile.Open(args.inFileNameMultiTrial)
 for iPt in range(1, nPtBins+1):
     ptMin = hRawYields.GetBinLowEdge(iPt)
     ptMax = ptMin+hRawYields.GetBinWidth(iPt)
     hRawYieldDistr.append(inFileMT.Get(f'hRawYield_pT_{ptMin*10:.0f}-{ptMax*10:.0f}'))
-    hRMS.SetBinContent(iPt, hRawYieldDistr[-1].GetRMS()/hRawYieldDistr[-1].GetMean()*100)
+    rms =  hRawYieldDistr[-1].GetRMS() / hRawYieldDistr[-1].GetMean() * 100
+    shift = TMath.Abs(hRawYields.GetBinContent(iPt) - hRawYieldDistr[-1].GetMean()) / hRawYieldDistr[-1].GetMean() * 100
+    syst = TMath.Sqrt(rms**2 + shift**2)
+    hRMS.SetBinContent(iPt, rms)
+    hMeanShift.SetBinContent(iPt, shift)
+    hSyst.SetBinContent(iPt, syst)
 
 cRMS = TCanvas('cRMS', '', 800, 800)
-cRMS.DrawFrame(hRMS.GetBinLowEdge(1), 0.01, ptMax, 20., ';#it{p}_{T} (GeV/#it{c});RMS (%)')
-cRMS.SetLogy()
+cRMS.DrawFrame(hRMS.GetBinLowEdge(1), 0.1, ptMax, 20., ';#it{p}_{T} (GeV/#it{c});RMS (%)')
 hRMS.DrawCopy('same')
 axisSoverB = TGaxis(gPad.GetUxmax(), gPad.GetUymin(), gPad.GetUxmax(), gPad.GetUymax(), 0.01, 20., 510, "+LG")
 axisSoverB.SetLineColor(kRed+1)
@@ -59,7 +67,21 @@ axisSoverB.SetTitleSize(0.05)
 axisSoverB.SetMaxDigits(3)
 axisSoverB.Draw()
 hSoverB.DrawCopy('same')
+cRMS.Update()
 
-cRMS.SaveAs(args.outFileName)
+cShift = TCanvas('cShift', '', 800, 800)
+cShift.DrawFrame(hMeanShift.GetBinLowEdge(1), 0.1, ptMax, 20., ';#it{p}_{T} (GeV/#it{c}); Mean Shift (%)')
+hMeanShift.DrawCopy('same')
+cShift.Update()
+
+cSyst = TCanvas('cSyst', '', 800, 800)
+cSyst.DrawFrame(hSyst.GetBinLowEdge(1), 0.1, ptMax, 20., ';#it{p}_{T} (GeV/#it{c}); Syst (%)')
+hSyst.DrawCopy('same')
+cSyst.Update()
+
+outFile = TFile(f'{args.outFileName}.root', 'recreate')
+cRMS.Write()
+cShift.Write()
+cSyst.Write()
 
 input('Press Enter to exit')
