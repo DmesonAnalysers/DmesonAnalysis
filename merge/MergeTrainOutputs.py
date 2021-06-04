584e844327d45b32fdef415f3b6708b4c3526747
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2021-06-04 19:06:16 +0200

    Fix path

--- merge/MergeTrainOutputs.py
+++ merge/MergeTrainOutputs.py
@@ -70,8 +70,9 @@ for iRun, run in enumerate(runs):
     print(f'\33[32mStart download of files for run {run}\33[0m')
     if not inputCfg['MergeOptions']['MergeByRun']:
         for inDir in inDirs[iRun]:
+            inName = os.path.join(dirName, f'{inDir}')
             outName = os.path.join(inputCfg["OutputPath"], f'{run}')
-            os.system(f'alien_cp -select {inputCfg["InputFileName"]} -y 2 -T 32 {dirName} file://{outName}/')
+            os.system(f'alien_cp -select {inputCfg["InputFileName"]} -y 2 -T 32 {inName} file://{outName}/')
     else:
         outName = os.path.join(inputCfg["OutputPath"], f'{run}')
         os.system(f'alien_cp -T 32 {dirName}/{inputCfg["InputFileName"]} file://{outName}/')
