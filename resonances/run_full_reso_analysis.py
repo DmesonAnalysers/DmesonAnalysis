c3b782faa6867012b539aadd003c0655856491d0
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2023-02-27 18:19:34 +0100

    Add flags to skip steps

--- resonances/run_full_reso_analysis.py
+++ resonances/run_full_reso_analysis.py
@@ -5,7 +5,11 @@ Script to run full analysis for D resonances
 import argparse
 import os
 
-def run_full_analysis(dir_config_proj, config_fit, trigger, pdg_d, pdg_v0):
+def run_full_analysis(dir_config_proj,
+                      config_fit, trigger,
+                      pdg_d, pdg_v0,
+                      skip_data_proj,
+                      skip_fit):
     """
     function for full analysis
 
@@ -17,6 +21,8 @@ def run_full_analysis(dir_config_proj, config_fit, trigger, pdg_d, pdg_v0):
     - trigger (str): trigger class (MB or HM)
     - pdg_d (int): PDG code of the D meson
     - pdg_v0 (int): PDG code of the V0
+    - skip_data_proj (bool): flag to skip data projection
+    - skip_fit (bool): flag to skip fit
     """
 
     for cfg_file in os.listdir(dir_config_proj):
@@ -42,17 +48,21 @@ def run_full_analysis(dir_config_proj, config_fit, trigger, pdg_d, pdg_v0):
             name_v0 = "Lambda"
 
         # projection of data
-        print("\n\033[92m Starting data projection\033[0m")
         command_proj = "python3 project_DV0reso.py"
         args_proj = f"{cfg_file} -t {trigger} -d {pdg_d} -v0 {pdg_v0} -o {dir_config_proj} {suffix_withopt}"
-        os.system(f"{command_proj} {args_proj}")
+        if not skip_data_proj:
+            print("\n\033[92m Starting data projection\033[0m")
+            print(f"\033[92m {command_proj} {args_proj}\033[0m")
+            os.system(f"{command_proj} {args_proj}")
 
         # raw yield extraction
-        print("\n\033[92m Starting raw yield extraction\033[0m")
         input_4fit = os.path.join(dir_config_proj, f"{name_d}_{name_v0}_{trigger}{suffix}.parquet.gzip")
         command_fit = "python3 extract_rawyield_DV0reso.py"
         args_fit = f"{input_4fit} {config_fit} -o {dir_config_proj} {suffix_withopt}"
-        os.system(f"{command_fit} {args_fit}")
+        if not skip_fit:
+            print("\n\033[92m Starting raw yield extraction\033[0m")
+            print(f"\033[92m {command_fit} {args_fit}\033[0m")
+            os.system(f"{command_fit} {args_fit}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Arguments")
@@ -66,6 +76,18 @@ if __name__ == "__main__":
                         help="pdg code of the D meson (options: [411, 413])")
     parser.add_argument("--pdg_V0", "-v0", type=int, required=True, default=310,
                         help="pdg code of the V0 (options: [310, 3122])")
+    parser.add_argument("--skip_data_proj", action="store_true", default=False,
+                        help="Skip data projection")
+    parser.add_argument("--skip_fit", action="store_true", default=False,
+                        help="Skip fit")
     args = parser.parse_args()
 
-    run_full_analysis(args.dir_config_proj, args.config_fit, args.trigger, args.pdg_D, args.pdg_V0)
+    run_full_analysis(
+        args.dir_config_proj,
+        args.config_fit,
+        args.trigger,
+        args.pdg_D,
+        args.pdg_V0,
+        args.skip_data_proj,
+        args.skip_fit        
+    )
