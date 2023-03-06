e76c88428395f49b2e0744d59168a1d70819a94d
stefanopolitano <stefano.politano@cern.ch>
2023-03-06 15:54:28 +0100

    Adding script to estimate statistical contribution to ry systematic

--- resonances/estimate_stat_in_ry_syst.py
+++ resonances/estimate_stat_in_ry_syst.py
@@ -1,81 +1,94 @@
 '''
-python script to generate the multitrial ditribution for systematic checks
-run: python generate_multitrial.py
+python script to estimate the statistical uncertainty in the systematic check
+run: python estimate_stat_in_ry_syst.py
 '''
 import sys
 import numpy as np
-from ROOT import TFile, TH1F, TGraphErrors, kRed, TF1, kBlack, TCanvas, TLatex
+from ROOT import TFile, TH1F, TGraphErrors, kRed, TF1, kBlack, TCanvas, TLatex, kBlue
 sys.path.insert(0, '..')
 from utils.StyleFormatter import SetGlobalStyle, SetObjectStyle
 SetGlobalStyle(padtopmargin=0.05, padleftmargin=0.18,padbottommargin=0.15,
                palette=55, labelsize=0.04, titlesize=0.06,
-               labeloffset=0.008, titleoffsety=1.55, titleoffsetx=1.2, titleoffsetz=1.,
+               labeloffset=0.008, titleoffsety=1.55, titleoffsetx=1., titleoffsetz=1.,
                opttitle=0, optstat=0)
 name_reso = "Ds2starplus"
-trigger = "HM"
-outlabel = ""
+trigger = "MB"
+outlabel = "_FINAL"
 
 #______________________________________________________________________________
 # Load reference file
-entries = [5, 30, 60, 70, 80] # HM: 5, 30, 60, 70, 80; MB: 1, 5, 10, 20, 30
-tfiles = []
+mults = [1, 20, 40, 100] if trigger == "MB" else [1, 10, 20, 40, 100] # at the moment exluding 10 for MB
 gRMS = TGraphErrors()
 SetObjectStyle(gRMS, color=kBlack, markerstyle=20, markersize=0.5)
-htrials = []
 rms, rms_unc = [], []
-for ientry, entry in enumerate(entries):
-    for trial in range(max(entries)):
-        if trigger == "HM" and trial in [12, 62, 78, 79, 80]: # skip bad trials HM: [5, 30, 60, 70, 80]
+hMult = []
+for i, mult in enumerate(mults):
+    meanval = 0.
+    meanval_unc = 0.
+    hMult.append(TH1F(f"hMult{mult}", f"hMult{i}", 50, 0, 0.5))
+    hMult[-1].SetDirectory(0)
+    for i in range(10):
+        if trigger == "HM" and i in [4]: # skip bad trials in HM (4)
             continue
-        if trial <= entry:
-            print(f"trial {trial} in range 0-{entry}")
-            tfile = TFile(f'./Multitrial_{name_reso}_pt2.0-24.0_{trigger}_test_bash_htrial_{trial}.root', "READ")
-            htrials.append(tfile.Get("h_syst"))
-            htrials[-1].SetDirectory(0)
-        else:
-            continue
+        file = TFile(f'./Multitrial_{name_reso}_pt2.0-24.0_{trigger}_test_bash_htrial{i}_multfact{mult:.1f}.root')
+        histo = file.Get('h_syst')
+        sigma = histo.GetRMS()
+        mean = histo.GetMean()
+        sigma_unc = histo.GetRMSError()
+        mean_unc = histo.GetMeanError()
+        hMult[-1].Fill(sigma/mean)
+    meanval = hMult[-1].GetMean()
+    meanval_unc = hMult[-1].GetMeanError()
+    rms.append(meanval)
+    rms_unc.append(meanval_unc)
 
-    if not htrials:
-        continue
-    else:
-        print(f"Found {len(htrials)} histograms")
-    hfinal = TH1F(f"hfinal_{entry}", "hfinal", 1000, 0, 1)
-    for ibin in range(1, hfinal.GetNbinsX()+1):
-        hfinal.SetBinContent(ibin, 0)
-    hfinal.SetDirectory(0)
-    SetObjectStyle(hfinal, color=kRed, markerstyle=20, markersize=0.5)
-    for h in htrials:
-        hfinal.Fill(h.GetRMS()/h.GetMean())
-    rms.append(hfinal.GetMean())
-    rms_unc.append(hfinal.GetMeanError()/np.sqrt(len(htrials)))
-    htrials = []
-
-f1 = TF1("f1", "[0] + [1]/sqrt(x)", 0, 100)
 canvas = TCanvas("canvas", "canvas", 800, 800)
-ymax = max(rms) + 0.05
+ymax = max(rms) + 0.1
 ymin = min(rms) - 0.02
-hFrame = canvas.DrawFrame(0, ymin, max(entries)+5, ymax, ";N_{trials};Average RMS/Mean")
-for i, (entry, rms, rms_unc) in enumerate(zip(entries, rms, rms_unc)):
-    gRMS.SetPoint(i, entry, rms)
+hFrame = canvas.DrawFrame(0, ymin, max(mults)+5, ymax, ";N_{evt}/N_{evt}(data);Average RMS/Mean")
+for i, (mult, rms, rms_unc) in enumerate(zip(mults, rms, rms_unc)):
+    gRMS.SetPoint(i, mult, rms)
     gRMS.SetPointError(i, 0, rms_unc)
+yhighfit = 30. if trigger == "HM" else 100.
+
+for mult in mults:
+    f1 = TF1(f"f1_multfact{mult}", "[0] + [1]/sqrt(x)", 1.e-2, mult+2)
+    gRMS.Fit(f1, "same r")
+    chi2 = f1.GetChisquare()
+    ndf = f1.GetNDF()
+    if ndf != 0:
+        red_chi2 = chi2/ndf
+        if red_chi2 > 5: # keep extending the fit range until the chi2/ndf is reasonable
+            print(f"\033[1;31mWARNING: red_chi2 = {red_chi2:.2f} > 2\033[0m")
+            continue
+        else:
+            print(f"red_chi2 = {red_chi2:.2f}")
+            yhighfit = mult+2
+f1 = TF1("f1", "[0] + [1]/sqrt(x)", 1.e-2, yhighfit)
+f1.SetLineColor(kRed+1)
+f1.SetLineStyle(2)
 gRMS.Draw('p same')
-gRMS.Fit(f1, "same")
+gRMS.Fit(f1, "same r")
 lat = TLatex()
 lat.SetTextSize(0.06)
 lat.SetNDC()
 lat.SetTextFont(42)
 lat.SetTextColor(kBlack)
 lat.DrawLatexNDC(0.2, 0.88, f'{name_reso} ({trigger})')
-lat.SetTextColor(kRed)
+lat.SetTextColor(kRed+1)
 lat.SetTextSize(0.04)
-lat.DrawLatexNDC(0.2, 0.82, f'f = p0 + p1/#sqrt{{#it{{N}}_{{trials}}}}')
-lat.DrawLatexNDC(0.2, 0.76, f'p0 = {f1.GetParameter(0):.3f} #pm {f1.GetParError(0):.3f}, p1 = {f1.GetParameter(1):.3f} #pm {f1.GetParError(1):.3f}')
+lat.DrawLatexNDC(0.2, 0.82, '#it{f} = p0 + p1/#sqrt{N_{evt}}')
+lat.DrawLatexNDC(0.2, 0.76, f'p0 = {f1.GetParameter(0):.3f} +- {f1.GetParError(0):.3f}, p1 = {f1.GetParameter(1):.3f} #pm {f1.GetParError(1):.3f}')
+lat.DrawLatexNDC(0.2, 0.20, f'#chi^{{2}}/ndf = {f1.GetChisquare()/f1.GetNDF():.2f}')
 canvas.Update()
+print(f'\033[1;32mINFO: RMS/Mean = {f1.GetParameter(1):.3f} +- {f1.GetParError(1):.3f}\033[0m')
+print(f'\033[1;32mINFO: SYST = {f1.GetParameter(0):.3f} +- {f1.GetParError(0):.3f}\033[0m')
 canvas.SaveAs(f"rysyst_statisticalcomponent_{name_reso}_{trigger}{outlabel}.pdf")
 outfile = TFile(f"rysyst_statisticalcomponent_{name_reso}_{trigger}{outlabel}.root", "RECREATE")
-for h in htrials:
+for h in hMult:
     h.Write()
 gRMS.Write()
 f1.Write()
+canvas.Write()
 outfile.Close()
 input()
