28fee3eb960073e8ce3408af679af2de8d03eac5
stefanopolitano <stefano.politano@cern.ch>
2023-03-06 16:55:38 +0100

    Updating uncertainty estimation in estimate_stat_in_ry_syst

--- resonances/estimate_stat_in_ry_syst.py
+++ resonances/estimate_stat_in_ry_syst.py
@@ -19,12 +19,10 @@ outlabel = "_FINAL"
 # Load reference file
 mults = [1, 20, 40, 100] if trigger == "MB" else [1, 10, 20, 40, 100] # at the moment exluding 10 for MB
 gRMS = TGraphErrors()
-SetObjectStyle(gRMS, color=kBlack, markerstyle=20, markersize=0.5)
+SetObjectStyle(gRMS, color=kBlack, markerstyle=20, markersize=1., linewidth=2)
 rms, rms_unc = [], []
 hMult = []
 for i, mult in enumerate(mults):
-    meanval = 0.
-    meanval_unc = 0.
     hMult.append(TH1F(f"hMult{mult}", f"hMult{i}", 50, 0, 0.5))
     hMult[-1].SetDirectory(0)
     for i in range(10):
@@ -34,11 +32,9 @@ for i, mult in enumerate(mults):
         histo = file.Get('h_syst')
         sigma = histo.GetRMS()
         mean = histo.GetMean()
-        sigma_unc = histo.GetRMSError()
-        mean_unc = histo.GetMeanError()
         hMult[-1].Fill(sigma/mean)
     rms.append(hMult[-1].GetMean())
-    rms_unc.append(hMult[-1].GetRMS()/mult)
+    rms_unc.append(hMult[-1].GetRMS())
 
 canvas = TCanvas("canvas", "canvas", 800, 800)
 ymax = max(rms) + 0.1
@@ -57,7 +53,7 @@ for mult in mults:
     if ndf != 0:
         red_chi2 = chi2/ndf
         if red_chi2 > 5: # keep extending the fit range until the chi2/ndf is reasonable
-            print(f"\033[1;31mWARNING: red_chi2 = {red_chi2:.2f} > 2\033[0m")
+            print(f"\033[1;31mWARNING: red_chi2 = {red_chi2:.2f} > 5\033[0m")
             continue
         else:
             print(f"red_chi2 = {red_chi2:.2f}")
