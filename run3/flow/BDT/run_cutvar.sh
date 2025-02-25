78f5504bdc89e2e3dc54e0b998a0509d28e667a0
marcellocosti <marcellodicostanzo00@gmail.com>
2025-02-25 13:27:01 +0100

    correct bold character in run_cutvar.sh

--- run3/flow/BDT/run_cutvar.sh
+++ run3/flow/BDT/run_cutvar.sh
@@ -27,16 +27,16 @@ export doddf=False
 export dov2vf=False
 export domergeimages=False
 
-export usepreprocessed=$([ "$usepreprocessed" = "false" ] && echo "" || echo "--use_preprocessed")
-export calc_weights=$([ "$docw" = "false" ] && echo "" || echo "--do_calc_weights")
-export make_yaml=$([ "$domy" = "false" ] && echo "" || echo "--do_make_yaml")
-export proj=$([ "$doproj" = "false" ] && echo "" || echo "--do_projections")
-export efficiency=$([ "$doeff" = "false" ] && echo "" || echo "--do_efficiency")
-export vn=$([ "$dovn" = "false" ] && echo "" || echo "--do_vn")
-export frac_cut_var=$([ "$dofcv" = "false" ] && echo "" || echo "--do_frac_cut_var")
-export data_driven_frac=$([ "$doddf" = "false" ] && echo "" || echo "--do_data_driven_frac")
-export v2_vs_frac=$([ "$dov2vf" = "false" ] && echo "" || echo "--do_v2_vs_frac")
-export merge_images=$([ "$domergeimages" = "false" ] && echo "" || echo "--do_merge_images")
+export usepreprocessed=$([ "$usepreprocessed" = "False" ] && echo "" || echo "--use_preprocessed")
+export calc_weights=$([ "$docw" = "False" ] && echo "" || echo "--do_calc_weights")
+export make_yaml=$([ "$domy" = "False" ] && echo "" || echo "--do_make_yaml")
+export proj=$([ "$doproj" = "False" ] && echo "" || echo "--do_projections")
+export efficiency=$([ "$doeff" = "False" ] && echo "" || echo "--do_efficiency")
+export vn=$([ "$dovn" = "False" ] && echo "" || echo "--do_vn")
+export frac_cut_var=$([ "$dofcv" = "False" ] && echo "" || echo "--do_frac_cut_var")
+export data_driven_frac=$([ "$doddf" = "False" ] && echo "" || echo "--do_data_driven_frac")
+export v2_vs_frac=$([ "$dov2vf" = "False" ] && echo "" || echo "--do_v2_vs_frac")
+export merge_images=$([ "$domergeimages" = "False" ] && echo "" || echo "--do_merge_images")
 
 python3 run_cutvar.py $config_flow \
 					  $usepreprocessed \
